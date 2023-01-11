c67f3d8e71e1d63d0ac4c0045074412a9dedf9c8
Silke Schmid <silke.schmid@muenchen.de>
2023-01-11 09:30:31 +0100

    Eintrag spring_oauth aus API-Doc entfernt, unnötige Imports entfernt

--- src/main/java/de/muenchen/isi/configuration/SwaggerConfiguration.java
+++ src/main/java/de/muenchen/isi/configuration/SwaggerConfiguration.java
@@ -4,47 +4,28 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.OAuthFlow;
-import io.swagger.v3.oas.models.security.OAuthFlows;
-import io.swagger.v3.oas.models.security.Scopes;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.Arrays;
-
-
 @Configuration
 @Profile({"local", "dev"})
 public class SwaggerConfiguration {
 
-    private final String authServer;
-
-    private final String realm;
-
     private final String buildVersion;
 
     @Autowired
-    public SwaggerConfiguration(@Value("${keycloak.auth-server-url}") final String authServer,
-                                @Value("${realm}") final String realm,
-                                @Value("${info.application.version}") final String buildVersion) {
-        this.authServer = authServer;
-        this.realm = realm;
+    public SwaggerConfiguration(@Value("${info.application.version}") final String buildVersion) {
         this.buildVersion = buildVersion;
     }
 
     @Bean
     public OpenAPI openAPI() {
-        final var authUrl = String.format("%s/realms/%s/protocol/openid-connect", this.authServer, this.realm);
 
         return new OpenAPI()
                 .components(new Components())
-                .security(Arrays.asList(
-                        new SecurityRequirement().addList("spring_oauth")))
                 .info(new Info()
                         .title("ISI Backend API")
                         .version(this.buildVersion)
