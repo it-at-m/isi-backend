fc7c0adc9cdecbe2413c7b129007d0eeab60bc22
boal <56300801+boal@users.noreply.github.com>
2023-02-13 08:13:03 +0100

    Update src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/GsNachmittagBetreuungService.java
    Co-authored-by: DomixD <dominik-grenz@t-online.de>

--- src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/GsNachmittagBetreuungService.java
+++ src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/GsNachmittagBetreuungService.java
@@ -68,7 +68,7 @@ public class GsNachmittagBetreuungService {
         try {
             gsNachmittagBetreuungEntity = this.gsNachmittagBetreuungRepository.saveAndFlush(gsNachmittagBetreuungEntity);
         } catch (final ObjectOptimisticLockingFailureException exception) {
-            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Daten neu";
+            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Seite neu!";
             throw new OptimisticLockingException(message, exception);
         }
         return this.infrastruktureinrichtungDomainMapper.entity2Model(gsNachmittagBetreuungEntity);
