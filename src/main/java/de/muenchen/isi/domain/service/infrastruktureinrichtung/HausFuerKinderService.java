54547e036b84f4aca64e5726b502c01f25de2360
boal <56300801+boal@users.noreply.github.com>
2023-02-13 08:17:40 +0100

    Update src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/HausFuerKinderService.java
    Co-authored-by: DomixD <dominik-grenz@t-online.de>

--- src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/HausFuerKinderService.java
+++ src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/HausFuerKinderService.java
@@ -68,7 +68,7 @@ public class HausFuerKinderService {
         try {
             hausFuerKinderEntity = this.hausFuerKinderRepository.saveAndFlush(hausFuerKinderEntity);
         } catch (final ObjectOptimisticLockingFailureException exception) {
-            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Daten neu";
+            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Seite neu!";
             throw new OptimisticLockingException(message, exception);
         }
         return this.infrastruktureinrichtungDomainMapper.entity2Model(hausFuerKinderEntity);
