8d24ed48210fb9eaac50a847ee15905227139865
boal <56300801+boal@users.noreply.github.com>
2023-02-13 08:09:07 +0100

    Update src/main/java/de/muenchen/isi/domain/service/AbfrageService.java
    Co-authored-by: DomixD <dominik-grenz@t-online.de>

--- src/main/java/de/muenchen/isi/domain/service/AbfrageService.java
+++ src/main/java/de/muenchen/isi/domain/service/AbfrageService.java
@@ -85,9 +85,9 @@ public class AbfrageService {
      *
      * @param abfrage zum Updaten
      * @return das geupdatete {@link InfrastrukturabfrageModel}
-     * @throws EntityNotFoundException    falls die Abfrage identifiziert durch die {@link InfrastrukturabfrageModel#getId()} nicht gefunden wird.
-     * @throws UniqueViolationException   falls der Name der Abfrage {@link InfrastrukturabfrageModel#getAbfrage().getNameAbfrage} ()} bereits vorhanden ist.
-     * @throws OptimisticLockingException falls in der Anwendung bereits eine neuere Version der Entität gespeichert ist.
+     * @throws EntityNotFoundException    falls die Abfrage identifiziert durch die {@link InfrastrukturabfrageModel#getId()} nicht gefunden wird
+     * @throws UniqueViolationException   falls der Name der Abfrage {@link InfrastrukturabfrageModel#getAbfrage().getNameAbfrage} ()} bereits vorhanden ist
+     * @throws OptimisticLockingException falls in der Anwendung bereits eine neuere Version der Entität gespeichert ist
      */
     public InfrastrukturabfrageModel updateInfrastrukturabfrage(final InfrastrukturabfrageModel abfrage) throws EntityNotFoundException, UniqueViolationException, OptimisticLockingException {
         this.getInfrastrukturabfrageById(abfrage.getId());
