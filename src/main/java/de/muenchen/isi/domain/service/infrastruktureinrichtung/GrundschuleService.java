d284b1ced17a8b6e6eeb3cf55e04514c2cf51270
boal <56300801+boal@users.noreply.github.com>
2023-02-13 08:15:23 +0100

    Update src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/GrundschuleService.java
    Co-authored-by: DomixD <dominik-grenz@t-online.de>

--- src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/GrundschuleService.java
+++ src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/GrundschuleService.java
@@ -68,7 +68,7 @@ public class GrundschuleService {
         try {
             grundschuleEntity = this.grundschuleRepository.saveAndFlush(grundschuleEntity);
         } catch (final ObjectOptimisticLockingFailureException exception) {
-            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Daten neu";
+            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Seite neu!";
             throw new OptimisticLockingException(message, exception);
         }
         return this.infrastruktureinrichtungDomainMapper.entity2Model(grundschuleEntity);
