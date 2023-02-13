8f245843b0fc8d60b03626b3b77ee9cbc390c671
boal <56300801+boal@users.noreply.github.com>
2023-02-13 08:09:58 +0100

    Update src/main/java/de/muenchen/isi/domain/service/stammdaten/FoerdermixStammService.java
    Co-authored-by: DomixD <dominik-grenz@t-online.de>

--- src/main/java/de/muenchen/isi/domain/service/stammdaten/FoerdermixStammService.java
+++ src/main/java/de/muenchen/isi/domain/service/stammdaten/FoerdermixStammService.java
@@ -62,7 +62,7 @@ public class FoerdermixStammService {
         try {
             entity = this.foerdermixStammRepository.saveAndFlush(entity);
         } catch (final ObjectOptimisticLockingFailureException exception) {
-            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Daten neu";
+            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Seite neu!";
             throw new OptimisticLockingException(message, exception);
         }
         return this.stammdatenDomainMapper.entity2Model(entity);
