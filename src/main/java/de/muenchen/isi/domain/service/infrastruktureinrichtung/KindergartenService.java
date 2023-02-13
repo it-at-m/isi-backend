55f0f27891484ee181a7ad320eedf6a1ba6f1aef
boal <56300801+boal@users.noreply.github.com>
2023-02-13 08:23:33 +0100

    Update src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/KindergartenService.java
    Co-authored-by: DomixD <dominik-grenz@t-online.de>

--- src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/KindergartenService.java
+++ src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/KindergartenService.java
@@ -68,7 +68,7 @@ public class KindergartenService {
         try {
             kindergartenEntity = this.kindergartenRepository.saveAndFlush(kindergartenEntity);
         } catch (final ObjectOptimisticLockingFailureException exception) {
-            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Daten neu";
+            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Seite neu!";
             throw new OptimisticLockingException(message, exception);
         }
         return this.infrastruktureinrichtungDomainMapper.entity2Model(kindergartenEntity);
