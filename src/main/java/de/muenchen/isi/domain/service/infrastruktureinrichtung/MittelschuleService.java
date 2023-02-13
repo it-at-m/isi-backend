88d73ebca010281230865e4778b8abaa74bea439
boal <56300801+boal@users.noreply.github.com>
2023-02-13 08:26:17 +0100

    Update src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/MittelschuleService.java
    Co-authored-by: DomixD <dominik-grenz@t-online.de>

--- src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/MittelschuleService.java
+++ src/main/java/de/muenchen/isi/domain/service/infrastruktureinrichtung/MittelschuleService.java
@@ -68,7 +68,7 @@ public class MittelschuleService {
         try {
             mittelschuleEntity = this.mittelschuleRepository.saveAndFlush(mittelschuleEntity);
         } catch (final ObjectOptimisticLockingFailureException exception) {
-            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Daten neu";
+            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Seite neu!";
             throw new OptimisticLockingException(message, exception);
         }
         return this.infrastruktureinrichtungDomainMapper.entity2Model(mittelschuleEntity);
