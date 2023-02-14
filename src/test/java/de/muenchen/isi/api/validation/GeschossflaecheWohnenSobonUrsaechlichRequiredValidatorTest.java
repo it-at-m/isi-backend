72d0ca90b13fd2d34a330a7e5376a2bf960b2e90
lhmAschaenz <119875041+lhmAschaenz@users.noreply.github.com>
2023-02-14 16:24:28 +0100

    ISI-171-Validierung-fuer-Geschossflaeche-SoBoN-ursaechlich-aufgenommen (#15)
    * ISI-171 Validierung für Geschossfläche SoBoN-ursächlich aufgenommen.
    
    * ISI-171 Review Anmerkungen eingearbeitet.
    
    ---------
    
    Co-authored-by: albrecht.schaenzel <albrecht.schaenzel@muenchen.de>

---
+++ src/test/java/de/muenchen/isi/api/validation/GeschossflaecheWohnenSobonUrsaechlichRequiredValidatorTest.java
@@ -0,0 +1,112 @@
+package de.muenchen.isi.api.validation;
+
+import de.muenchen.isi.api.dto.AbfragevarianteDto;
+import de.muenchen.isi.api.dto.InfrastrukturabfrageDto;
+import de.muenchen.isi.infrastructure.entity.enums.lookup.Planungsrecht;
+import de.muenchen.isi.infrastructure.entity.enums.lookup.UncertainBoolean;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class GeschossflaecheWohnenSobonUrsaechlichRequiredValidatorTest {
+
+    private final GeschossflaecheWohnenSobonUrsaechlichRequiredValidator geschossflaecheWohnenSobonUrsaechlichRequiredValidator = new GeschossflaecheWohnenSobonUrsaechlichRequiredValidator();
+
+    @Test
+    void isValidSobonRelevant() {
+        final InfrastrukturabfrageDto infrastrukturabfrage = new InfrastrukturabfrageDto();
+        infrastrukturabfrage.setSobonRelevant(UncertainBoolean.TRUE);
+        assertThat(
+                this.geschossflaecheWohnenSobonUrsaechlichRequiredValidator.isValid(infrastrukturabfrage, null),
+                is(true)
+        );
+
+        infrastrukturabfrage.setAbfragevarianten(new ArrayList<AbfragevarianteDto>());
+
+        final AbfragevarianteDto abfragevariantePlanungsrechtParag11 = new AbfragevarianteDto();
+        abfragevariantePlanungsrechtParag11.setPlanungsrecht(Planungsrecht.BPLAN_PARAG_11);
+        abfragevariantePlanungsrechtParag11.setGeschossflaecheWohnenSoBoNursaechlich(BigDecimal.TEN);
+        infrastrukturabfrage.getAbfragevarianten().add(abfragevariantePlanungsrechtParag11);
+        assertThat(
+                this.geschossflaecheWohnenSobonUrsaechlichRequiredValidator.isValid(infrastrukturabfrage, null),
+                is(true)
+        );
+
+        final AbfragevarianteDto abfragevariantePlanungsrechtParag12 = new AbfragevarianteDto();
+        abfragevariantePlanungsrechtParag12.setPlanungsrecht(Planungsrecht.BPLAN_PARAG_12);
+        abfragevariantePlanungsrechtParag12.setGeschossflaecheWohnenSoBoNursaechlich(BigDecimal.ZERO);
+        infrastrukturabfrage.getAbfragevarianten().add(abfragevariantePlanungsrechtParag12);
+        assertThat(
+                this.geschossflaecheWohnenSobonUrsaechlichRequiredValidator.isValid(infrastrukturabfrage, null),
+                is(true)
+        );
+
+        abfragevariantePlanungsrechtParag12.setGeschossflaecheWohnenSoBoNursaechlich(null);
+        assertThat(
+                this.geschossflaecheWohnenSobonUrsaechlichRequiredValidator.isValid(infrastrukturabfrage, null),
+                is(false)
+        );
+
+    }
+
+    @Test
+    void isValidAbfrageNotSobonRelevant() {
+        final InfrastrukturabfrageDto infrastrukturabfrage = new InfrastrukturabfrageDto();
+        infrastrukturabfrage.setSobonRelevant(UncertainBoolean.FALSE);
+        assertThat(
+                this.geschossflaecheWohnenSobonUrsaechlichRequiredValidator.isValid(infrastrukturabfrage, null),
+                is(true)
+        );
+
+        infrastrukturabfrage.setAbfragevarianten(new ArrayList<AbfragevarianteDto>());
+
+        final AbfragevarianteDto abfragevariantePlanungsrechtParag11 = new AbfragevarianteDto();
+        abfragevariantePlanungsrechtParag11.setPlanungsrecht(Planungsrecht.BPLAN_PARAG_11);
+        abfragevariantePlanungsrechtParag11.setGeschossflaecheWohnenSoBoNursaechlich(BigDecimal.TEN);
+        infrastrukturabfrage.getAbfragevarianten().add(abfragevariantePlanungsrechtParag11);
+        assertThat(
+                this.geschossflaecheWohnenSobonUrsaechlichRequiredValidator.isValid(infrastrukturabfrage, null),
+                is(true)
+        );
+
+        final AbfragevarianteDto abfragevariantePlanungsrechtParag12 = new AbfragevarianteDto();
+        abfragevariantePlanungsrechtParag12.setPlanungsrecht(Planungsrecht.BPLAN_PARAG_12);
+        abfragevariantePlanungsrechtParag12.setGeschossflaecheWohnenSoBoNursaechlich(null);
+        infrastrukturabfrage.getAbfragevarianten().add(abfragevariantePlanungsrechtParag12);
+        assertThat(
+                this.geschossflaecheWohnenSobonUrsaechlichRequiredValidator.isValid(infrastrukturabfrage, null),
+                is(true)
+        );
+    }
+
+    @Test
+    void isValidAbfragevarianteNotSobonRelevant() {
+        final InfrastrukturabfrageDto infrastrukturabfrage = new InfrastrukturabfrageDto();
+        infrastrukturabfrage.setSobonRelevant(UncertainBoolean.TRUE);
+        assertThat(
+                this.geschossflaecheWohnenSobonUrsaechlichRequiredValidator.isValid(infrastrukturabfrage, null),
+                is(true)
+        );
+
+        infrastrukturabfrage.setAbfragevarianten(new ArrayList<AbfragevarianteDto>());
+
+        final AbfragevarianteDto abfragevariantePlanungsrechtParag30 = new AbfragevarianteDto();
+        abfragevariantePlanungsrechtParag30.setPlanungsrecht(Planungsrecht.BPLAN_PARAG_30);
+        abfragevariantePlanungsrechtParag30.setGeschossflaecheWohnenSoBoNursaechlich(BigDecimal.TEN);
+        infrastrukturabfrage.getAbfragevarianten().add(abfragevariantePlanungsrechtParag30);
+
+        final AbfragevarianteDto abfragevariantePlanungsrechtParag34 = new AbfragevarianteDto();
+        abfragevariantePlanungsrechtParag34.setPlanungsrecht(Planungsrecht.NACHVERD_PARAG_34);
+        infrastrukturabfrage.getAbfragevarianten().add(abfragevariantePlanungsrechtParag34);
+
+        assertThat(
+                this.geschossflaecheWohnenSobonUrsaechlichRequiredValidator.isValid(infrastrukturabfrage, null),
+                is(true)
+        );
+    }
+
+}
