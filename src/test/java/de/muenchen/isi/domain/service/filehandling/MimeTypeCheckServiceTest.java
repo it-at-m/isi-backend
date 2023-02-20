2f5ca65ea71859f558046571ddfad313da9504ee
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-02-20 17:08:57 +0100

    add closing of stream in parameter

--- src/test/java/de/muenchen/isi/domain/service/filehandling/MimeTypeCheckServiceTest.java
+++ src/test/java/de/muenchen/isi/domain/service/filehandling/MimeTypeCheckServiceTest.java
@@ -22,13 +22,26 @@ class MimeTypeCheckServiceTest {
 
     @Test
     void getMediaTypeOfFileAndCloseStream() throws IOException, MimeTypeException {
-        final InputStream file = this.getClass().getClassLoader().getResourceAsStream("dummy_for_test.pdf");
-        final var result = this.mimeTypeCheckService.getMediaTypeInformationOfFileAndCloseStream(file);
+        InputStream file = this.getClass().getClassLoader().getResourceAsStream("pdf_for_test.pdf");
+        var result = this.mimeTypeCheckService.getMediaTypeInformationOfFileAndCloseStream(file);
 
-        final var expected = new MimeTypeCheckService.MediaTypeInformation();
+        var expected = new MimeTypeCheckService.MediaTypeInformation();
         expected.setType("application/pdf");
         expected.setDescription("Portable Document Format");
         expected.setAcronym("PDF");
+
+        assertThat(
+                result,
+                is(expected)
+        );
+
+        file = this.getClass().getClassLoader().getResourceAsStream("svg_for_test.svg");
+        result = this.mimeTypeCheckService.getMediaTypeInformationOfFileAndCloseStream(file);
+
+        expected = new MimeTypeCheckService.MediaTypeInformation();
+        expected.setType("image/svg+xml");
+        expected.setDescription("Scalable Vector Graphics");
+        expected.setAcronym("SVG");
 
         assertThat(
                 result,
