15a8cab1ac769b98f30640d427b223adf0466b21
fatih.uguz <fatih.uguz@muenchen.de>
2023-02-21 10:32:27 +0100

    ISI-321 UniqueConstraint im FoerdermixStamm ergaenzt

--- src/main/java/de/muenchen/isi/infrastructure/entity/stammdaten/FoerdermixStamm.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/stammdaten/FoerdermixStamm.java
@@ -13,9 +13,16 @@ import lombok.ToString;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Data
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"bezeichnung", "bezeichnungJahr"})
+        }
+)
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class FoerdermixStamm extends BaseEntity {
