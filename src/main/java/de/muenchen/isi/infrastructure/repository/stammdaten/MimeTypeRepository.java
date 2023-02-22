d7762b69e103a4ef04addd7cd8618341de34938e
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-02-22 10:30:18 +0100

    adapt mimetype repo

--- src/main/java/de/muenchen/isi/infrastructure/repository/stammdaten/MimeTypeRepository.java
+++ src/main/java/de/muenchen/isi/infrastructure/repository/stammdaten/MimeTypeRepository.java
@@ -2,45 +2,43 @@ package de.muenchen.isi.infrastructure.repository.stammdaten;
 
 import de.muenchen.isi.infrastructure.entity.stammdaten.MimeTypeInformation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.core.io.ResourceLoader;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.mime.MimeTypeException;
+import org.apache.tika.mime.MimeTypes;
 import org.springframework.stereotype.Repository;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
  * Diese Klasse hält für einen MIME-Type die möglichen Dateiendungen vor.
- * <p>
- * Die Basis dieser Zuordnung bildet die Datei "mime.types" in den Ressourcen.
- * <p>
- * Die jeweils aktuellste Datei kann unter folgender URL bezogen werden:
- * http://svn.apache.org/viewvc/httpd/httpd/trunk/docs/conf/mime.types?view=markup
  */
 @Repository
 @Slf4j
 public class MimeTypeRepository {
 
-    private static final String REGEX_MULTIPLE_WHITESPACE = "\\s+";
-
-    private final Map<String, MimeTypeInformation> mimeTypeFileExtensionsMap;
-
-    private final ResourceLoader resourceLoader;
-
-    public MimeTypeRepository(final ResourceLoader resourceLoader) throws IOException {
-        this.mimeTypeFileExtensionsMap = new HashMap<>();
-        this.resourceLoader = resourceLoader;
-        this.init();
-    }
-
     public Optional<MimeTypeInformation> findByMimeType(final String mimeType) {
-        return Optional.ofNullable(this.mimeTypeFileExtensionsMap.get(mimeType));
+        final TikaConfig config = TikaConfig.getDefaultConfig();
+        final MimeTypes mimeTypesRepository = config.getMimeRepository();
+        Optional<MimeTypeInformation> mimeTypeInformationResult;
+        try {
+            final var foundMimeType = mimeTypesRepository.getRegisteredMimeType(mimeType);
+            if (ObjectUtils.isNotEmpty(foundMimeType)) {
+                final var mimeTypeInformation = new MimeTypeInformation();
+                mimeTypeInformation.setMimeType(mimeType);
+                mimeTypeInformation.setFileExtensions(foundMimeType.getExtensions());
+                mimeTypeInformationResult = Optional.of(mimeTypeInformation);
+            } else {
+                mimeTypeInformationResult = Optional.empty();
+            }
+        } catch (final MimeTypeException exception) {
+            final var message = String.format("Der MimeType %s ist ungültig.", mimeType);
+            log.error(message);
+            mimeTypeInformationResult = Optional.empty();
+        }
+        return mimeTypeInformationResult;
     }
 
     public List<MimeTypeInformation> findAllByMimeTypes(final List<String> mimeTypes) {
@@ -49,54 +47,6 @@ public class MimeTypeRepository {
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .collect(Collectors.toList());
-    }
-
-    /**
-     * Die in den Ressourcen abgelegte Datei repräsentiert die gängigsten MIME-Types und deren Dateiendungen.
-     * <p>
-     * http://svn.apache.org/viewvc/httpd/httpd/trunk/docs/conf/mime.types?view=markup
-     * <p>
-     * Diese Methode erstellt je Zeile in der Datei ein {@link MimeTypeInformation} und legt diese im Repository ab.
-     */
-    protected void init() throws IOException {
-        final var mimeInputStream = this.resourceLoader.getResource("classpath:mime.types").getInputStream();
-        IOUtils.readLines(new InputStreamReader(mimeInputStream)).stream()
-                .filter(StringUtils::isNotBlank)
-                .filter(line -> !StringUtils.startsWith("#", line))
-                .map(this::parseLineToMimeTypeInfos)
-                .forEach(mimeTypeInformation ->
-                        this.mimeTypeFileExtensionsMap.put(
-                                mimeTypeInformation.getMimeType(),
-                                mimeTypeInformation
-                        )
-                );
-        log.info("MIME-Type-Infos sind geladen.");
-    }
-
-    /**
-     * Erstellt aus der verarbeitbaren Zeile ein {@link MimeTypeInformation}.
-     * <p>
-     * Eine Zeile startet mit dem MIME-Type und wird ergänzt durch die möglichen Dateiendungen ohne führenden Punkt.
-     * Die einzelnen Zeilenabschnitte sind durch Tabs oder Leerzeichen getrennt.
-     * <p>
-     * Beispiele:
-     * image/jpeg					jpeg jpg jpe
-     * image/svg+xml					svg svgz
-     * application/pdf					pdf
-     *
-     * @param line als verarbeitbare Zeile der mime.type-Datei in den Resourcen. Darf nicht null sein.
-     * @return die {@link MimeTypeInformation} der Zeile.
-     * @throws NullPointerException falls der Paramter den Wert null besitzt.
-     */
-    protected MimeTypeInformation parseLineToMimeTypeInfos(final String line) throws NullPointerException {
-        final var lineElements = List.of(line.split(REGEX_MULTIPLE_WHITESPACE));
-        final var fileExtensionsWithPointPrefix = lineElements.subList(1, lineElements.size()).stream()
-                .map(fileExtensionWithoutPointPrefix -> "." + fileExtensionWithoutPointPrefix)
-                .collect(Collectors.toList());
-        final var mimeTypeInfos = new MimeTypeInformation();
-        mimeTypeInfos.setMimeType(lineElements.get(0));
-        mimeTypeInfos.setFileExtensions(fileExtensionsWithPointPrefix);
-        return mimeTypeInfos;
     }
 
 }
--- src/test/java/de/muenchen/isi/infrastructure/repository/stammdaten/MimeTypeRepositoryTest.java
+++ src/test/java/de/muenchen/isi/infrastructure/repository/stammdaten/MimeTypeRepositoryTest.java
@@ -1,21 +1,13 @@
 package de.muenchen.isi.infrastructure.repository.stammdaten;
 
 import de.muenchen.isi.infrastructure.entity.stammdaten.MimeTypeInformation;
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,25 +18,10 @@ import static org.hamcrest.Matchers.is;
 @MockitoSettings(strictness = Strictness.LENIENT)
 class MimeTypeRepositoryTest {
 
-    private MimeTypeRepository mimeTypeRepository;
-
-    @Mock
-    private ResourceLoader resourceLoader;
-
-    @Mock
-    private Resource resource;
-
-    @BeforeEach
-    public void beforeEach() throws IOException {
-        Mockito.when(this.resourceLoader.getResource("classpath:mime.types")).thenReturn(this.resource);
-        final var inputStream = IOUtils.toInputStream(this.getFileContent(), Charset.defaultCharset());
-        Mockito.when(this.resource.getInputStream()).thenReturn(inputStream);
-        this.mimeTypeRepository = new MimeTypeRepository(this.resourceLoader);
-        Mockito.reset(this.resourceLoader, this.resource);
-    }
+    private final MimeTypeRepository mimeTypeRepository = new MimeTypeRepository();
 
     @Test
-    void init() {
+    void findByMimeType() {
         var result = this.mimeTypeRepository.findByMimeType("application/andrew-inset");
         var expected = new MimeTypeInformation();
         expected.setMimeType("application/andrew-inset");
@@ -98,103 +75,34 @@ class MimeTypeRepositoryTest {
     }
 
     @Test
-    void parseLineToMimeTypeInfos() {
-        var lineOfFile = "image/vnd.ms-modi\t\t\t\tmdi";
-        var result = this.mimeTypeRepository.parseLineToMimeTypeInfos(lineOfFile);
-        var expected = new MimeTypeInformation();
-        expected.setMimeType("image/vnd.ms-modi");
-        expected.setFileExtensions(List.of(".mdi"));
-        assertThat(
-                result,
-                is(expected)
+    void findAllByMimeTypes() {
+        final var result = this.mimeTypeRepository.findAllByMimeTypes(
+                List.of(
+                        "application/andrew-inset",
+                        "application/atomcat+xml",
+                        "no/mimetype",
+                        "application/mathematica"
+                )
         );
 
-        lineOfFile = "image/vnd.ms-modi\t        \t\tmdi";
-        result = this.mimeTypeRepository.parseLineToMimeTypeInfos(lineOfFile);
-        expected = new MimeTypeInformation();
-        expected.setMimeType("image/vnd.ms-modi");
-        expected.setFileExtensions(List.of(".mdi"));
+        final var expectedList = new ArrayList<MimeTypeInformation>();
+        var mimeTypeInformation = new MimeTypeInformation();
+        mimeTypeInformation.setMimeType("application/andrew-inset");
+        mimeTypeInformation.setFileExtensions(List.of(".ez"));
+        expectedList.add(mimeTypeInformation);
+        mimeTypeInformation = new MimeTypeInformation();
+        mimeTypeInformation.setMimeType("application/atomcat+xml");
+        mimeTypeInformation.setFileExtensions(List.of(".atomcat"));
+        expectedList.add(mimeTypeInformation);
+        mimeTypeInformation = new MimeTypeInformation();
+        mimeTypeInformation.setMimeType("application/mathematica");
+        mimeTypeInformation.setFileExtensions(List.of(".ma", ".nb", ".mb"));
+        expectedList.add(mimeTypeInformation);
+
         assertThat(
                 result,
-                is(expected)
+                is(expectedList)
         );
-
-        lineOfFile = "image/vnd.ms-modi mdi";
-        result = this.mimeTypeRepository.parseLineToMimeTypeInfos(lineOfFile);
-        expected = new MimeTypeInformation();
-        expected.setMimeType("image/vnd.ms-modi");
-        expected.setFileExtensions(List.of(".mdi"));
-        assertThat(
-                result,
-                is(expected)
-        );
-
-        lineOfFile = "image/vnd.ms-modi            mdi";
-        result = this.mimeTypeRepository.parseLineToMimeTypeInfos(lineOfFile);
-        expected = new MimeTypeInformation();
-        expected.setMimeType("image/vnd.ms-modi");
-        expected.setFileExtensions(List.of(".mdi"));
-        assertThat(
-                result,
-                is(expected)
-        );
-
-
-        lineOfFile = "image/jpeg\t\t\t\t\tjpeg jpg \t jpe";
-        result = this.mimeTypeRepository.parseLineToMimeTypeInfos(lineOfFile);
-        expected = new MimeTypeInformation();
-        expected.setMimeType("image/jpeg");
-        expected.setFileExtensions(List.of(".jpeg", ".jpg", ".jpe"));
-        assertThat(
-                result,
-                is(expected)
-        );
-
-        Assertions.assertThrows(NullPointerException.class, () -> this.mimeTypeRepository.parseLineToMimeTypeInfos(null));
-    }
-
-    private String getFileContent() {
-        return "# This file maps Internet media types to unique file extension(s).\n" +
-                "# Although created for httpd, this file is used by many software systems\n" +
-                "# and has been placed in the public domain for unlimited redisribution.\n" +
-                "#\n" +
-                "# The table below contains both registered and (common) unregistered types.\n" +
-                "# A type that has no unique extension can be ignored -- they are listed\n" +
-                "# here to guide configurations toward known types and to make it easier to\n" +
-                "# identify \"new\" types.  File extensions are also commonly used to indicate\n" +
-                "# content languages and encodings, so choose them carefully.\n" +
-                "#\n" +
-                "# Internet media types should be registered as described in RFC 4288.\n" +
-                "# The registry is at <http://www.iana.org/assignments/media-types/>.\n" +
-                "#\n" +
-                "# MIME type (lowercased)\t\t\tExtensions\n" +
-                "# ============================================\t==========\n" +
-                "# application/1d-interleaved-parityfec\n" +
-                "# application/3gpdash-qoe-report+xml\n" +
-                "# application/3gpp-ims+xml\n" +
-                "# application/a2l\n" +
-                "# application/activemessage\n" +
-                "# application/alto-costmap+json\n" +
-                "# application/alto-costmapfilter+json\n" +
-                "# application/alto-directory+json\n" +
-                "# application/alto-endpointcost+json\n" +
-                "# application/alto-endpointcostparams+json\n" +
-                "# application/alto-endpointprop+json\n" +
-                "# application/alto-endpointpropparams+json\n" +
-                "# application/alto-error+json\n" +
-                "# application/alto-networkmap+json\n" +
-                "# application/alto-networkmapfilter+json\n" +
-                "# application/aml\n" +
-                "application/andrew-inset\t\t\tez\n" +
-                "# application/applefile\n" +
-                "application/applixware\t\t\t\taw\n" +
-                "# application/atf\n" +
-                "# application/atfx\n" +
-                "application/atom+xml\t\t\t\tatom\n" +
-                "application/atomcat+xml\t\t\t\tatomcat\n" +
-                "application/mathematica\t\t\t\tma nb mb" +
-                "\n" +
-                "";
     }
 
 }
