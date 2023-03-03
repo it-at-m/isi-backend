4af8d284dad6a2621304687288953c429b77f421
fatih.uguz <fatih.uguz@muenchen.de>
2023-03-03 17:40:26 +0100

    ISI-394 @Min-Annotation fuer die Anteile des Foerdermixes hinzugefuegt
    Somit sollen Negativwerte auch im Backend nochmal Validiert werden.

--- src/main/java/de/muenchen/isi/api/dto/FoerdermixDto.java
+++ src/main/java/de/muenchen/isi/api/dto/FoerdermixDto.java
@@ -6,23 +6,31 @@ package de.muenchen.isi.api.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import java.math.BigDecimal;
 
 @Data
 public class FoerdermixDto {
 
+    @Min(0)
     private BigDecimal anteilFreifinanzierterGeschosswohnungsbau;
 
+    @Min(0)
     private BigDecimal anteilGefoerderterMietwohnungsbau;
 
+    @Min(0)
     private BigDecimal anteilMuenchenModell;
 
+    @Min(0)
     private BigDecimal anteilPreisgedaempfterMietwohnungsbau;
 
+    @Min(0)
     private BigDecimal anteilKonzeptionellerMietwohnungsbau;
 
+    @Min(0)
     private BigDecimal anteilBaugemeinschaften;
 
+    @Min(0)
     private BigDecimal anteilEinUndZweifamilienhaeuser;
 
 }
