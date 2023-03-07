36ae8ab27bdfdd481c0964839f6b7bd0bda451ee
fatih.uguz <fatih.uguz@muenchen.de>
2023-03-07 17:20:10 +0100

    ISI-321 Anpassung von Response-Messages

--- src/main/java/de/muenchen/isi/api/controller/stammdaten/FoerdermixStammController.java
+++ src/main/java/de/muenchen/isi/api/controller/stammdaten/FoerdermixStammController.java
@@ -82,7 +82,7 @@ public class FoerdermixStammController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "CREATED -> FoerdermixStamm wurde erfolgreich erstellt."),
             @ApiResponse(responseCode = "400", description = "BAD_REQUEST -> FoerdermixStamm konnte nicht erstellt werden, überprüfen sie die Eingabe.", content = @Content(schema = @Schema(implementation = InformationResponseDto.class))),
-            @ApiResponse(responseCode = "409", description = "CONFLICT -> Fördermix konnte nicht erstellt werden, da die Bezeichnung im angegebenen Jahr bereits existiert.", content = @Content(schema = @Schema(implementation = InformationResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "CONFLICT -> Fördermix konnte nicht erstellt werden, da für die Bezeichnung im angegebenen Jahr bereits ein anderer Fördermix existiert.", content = @Content(schema = @Schema(implementation = InformationResponseDto.class))),
             @ApiResponse(responseCode = "412", description = "PRECONDITION_FAILED -> In der Anwendung ist bereits eine neuere Version der Entität gespeichert.", content = @Content(schema = @Schema(implementation = InformationResponseDto.class)))
     })
     @PreAuthorize("hasAuthority(T(de.muenchen.isi.security.AuthoritiesEnum).ISI_BACKEND_WRITE_STAMMDATEN_FOERDERMIX.name())")
@@ -99,7 +99,7 @@ public class FoerdermixStammController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK -> FoerdermixStamm wurde erfolgreich aktualisiert."),
             @ApiResponse(responseCode = "404", description = "NOT_FOUND -> Es gibt keine FoerdermixStamm mit der ID.", content = @Content(schema = @Schema(implementation = InformationResponseDto.class))),
-            @ApiResponse(responseCode = "409", description = "CONFLICT -> Fördermix konnte nicht erstellt werden, da die Bezeichnung im angegebenen Jahr bereits existiert.", content = @Content(schema = @Schema(implementation = InformationResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "CONFLICT -> Fördermix konnte nicht gespeichert werden, da für die Bezeichnung im angegebenen Jahr bereits ein anderer Fördermix existiert.", content = @Content(schema = @Schema(implementation = InformationResponseDto.class))),
             @ApiResponse(responseCode = "412", description = "PRECONDITION_FAILED -> In der Anwendung ist bereits eine neuere Version der Entität gespeichert.", content = @Content(schema = @Schema(implementation = InformationResponseDto.class)))
     })
     @PreAuthorize("hasAuthority(T(de.muenchen.isi.security.AuthoritiesEnum).ISI_BACKEND_WRITE_STAMMDATEN_FOERDERMIX.name())")
