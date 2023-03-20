af350e474cd22e05b06a03a7590c072ef475eca5
Dominik Grenz <dominik.grenz@muenchen.de>
2023-03-20 08:47:13 +0100

    Fix transient error

--- src/main/java/de/muenchen/isi/infrastructure/entity/Foerdermix.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/Foerdermix.java
@@ -5,7 +5,10 @@
 package de.muenchen.isi.infrastructure.entity;
 
 import lombok.Data;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.util.List;
 
@@ -13,7 +16,7 @@ import java.util.List;
 @Embeddable
 public class Foerdermix {
 
-    @OneToMany
+    @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER, orphanRemoval = true)
     private List<Foerderart> foerderarten;
 
 }
