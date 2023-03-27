1d42267d1f7c18d597d79d6e2aba2eb1f1f2f5b6
Dominik Grenz <dominik.grenz@muenchen.de>
2023-03-27 12:33:14 +0200

    Update tests

--- src/test/java/de/muenchen/isi/api/validation/HasFoerdermixRequiredSumValidatorTest.java
+++ src/test/java/de/muenchen/isi/api/validation/HasFoerdermixRequiredSumValidatorTest.java
@@ -1,9 +1,14 @@
 package de.muenchen.isi.api.validation;
 
+import de.muenchen.isi.api.dto.FoerderartDto;
 import de.muenchen.isi.api.dto.FoerdermixDto;
+import de.muenchen.isi.infrastructure.entity.Foerderart;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -15,78 +20,57 @@ class HasFoerdermixRequiredSumValidatorTest {
     @Test
     void isValid() {
         var foerdermix = new FoerdermixDto();
-        foerdermix.setAnteilFreifinanzierterGeschosswohnungsbau(BigDecimal.valueOf(60));
-        foerdermix.setAnteilGefoerderterMietwohnungsbau(BigDecimal.valueOf(40.00));
-        foerdermix.setAnteilMuenchenModell(BigDecimal.valueOf(0.00));
-        foerdermix.setAnteilPreisgedaempfterMietwohnungsbau(BigDecimal.ZERO);
-        foerdermix.setAnteilKonzeptionellerMietwohnungsbau(BigDecimal.valueOf(0));
-        foerdermix.setAnteilBaugemeinschaften(BigDecimal.valueOf(0));
-        foerdermix.setAnteilEinUndZweifamilienhaeuser(BigDecimal.valueOf(0));
+        FoerderartDto foerderart = new FoerderartDto();
+        foerderart.setBezeichnung("AnteilMuenchenModell");
+        foerderart.setAnteilProzent(BigDecimal.valueOf(40));
+
+        FoerderartDto foerderart2 = new FoerderartDto();
+        foerderart2.setBezeichnung("AnteilBaugemeinschaft");
+        foerderart2.setAnteilProzent(BigDecimal.valueOf(60));
+
+        List<FoerderartDto> foerderarten = new ArrayList<>(Arrays.asList(foerderart, foerderart2));
+        foerdermix.setFoerderarten(foerderarten);
+
         assertThat(
                 this.hasFoerdermixRequiredSumValidator.isValid(foerdermix, null),
                 is(true)
         );
 
-        foerdermix = new FoerdermixDto();
-        foerdermix.setAnteilFreifinanzierterGeschosswohnungsbau(null);
-        foerdermix.setAnteilGefoerderterMietwohnungsbau(BigDecimal.valueOf(99.99));
-        foerdermix.setAnteilMuenchenModell(BigDecimal.valueOf(0.01));
-        foerdermix.setAnteilPreisgedaempfterMietwohnungsbau(null);
-        foerdermix.setAnteilKonzeptionellerMietwohnungsbau(null);
-        foerdermix.setAnteilBaugemeinschaften(null);
-        foerdermix.setAnteilEinUndZweifamilienhaeuser(BigDecimal.valueOf(0));
+
+        foerderart.setAnteilProzent(BigDecimal.valueOf(99.99));
+        foerderart2.setAnteilProzent(BigDecimal.valueOf(0.01));
+
         assertThat(
                 this.hasFoerdermixRequiredSumValidator.isValid(foerdermix, null),
                 is(true)
         );
 
-        foerdermix = new FoerdermixDto();
-        foerdermix.setAnteilFreifinanzierterGeschosswohnungsbau(null);
-        foerdermix.setAnteilGefoerderterMietwohnungsbau(BigDecimal.valueOf(99.99));
-        foerdermix.setAnteilMuenchenModell(BigDecimal.valueOf(0.01));
-        foerdermix.setAnteilPreisgedaempfterMietwohnungsbau(null);
-        foerdermix.setAnteilKonzeptionellerMietwohnungsbau(null);
-        foerdermix.setAnteilBaugemeinschaften(null);
-        foerdermix.setAnteilEinUndZweifamilienhaeuser(BigDecimal.valueOf(0));
+
+        FoerderartDto foerderart3 = new FoerderartDto();
+        foerderart3.setBezeichnung("AnteilEinUndZweifamilienhaeuser");
+        foerderart3.setAnteilProzent(BigDecimal.valueOf(0));
+
+        foerderarten = new ArrayList<>(Arrays.asList(foerderart, foerderart2,foerderart3));
+        foerdermix.setFoerderarten(foerderarten);
+
         assertThat(
                 this.hasFoerdermixRequiredSumValidator.isValid(foerdermix, null),
                 is(true)
         );
 
-        foerdermix = new FoerdermixDto();
-        foerdermix.setAnteilFreifinanzierterGeschosswohnungsbau(null);
-        foerdermix.setAnteilGefoerderterMietwohnungsbau(BigDecimal.valueOf(99.99));
-        foerdermix.setAnteilMuenchenModell(BigDecimal.valueOf(0.02));
-        foerdermix.setAnteilPreisgedaempfterMietwohnungsbau(null);
-        foerdermix.setAnteilKonzeptionellerMietwohnungsbau(null);
-        foerdermix.setAnteilBaugemeinschaften(null);
-        foerdermix.setAnteilEinUndZweifamilienhaeuser(BigDecimal.valueOf(0));
+
+        foerderart2.setAnteilProzent(BigDecimal.valueOf(0.02));
+
         assertThat(
                 this.hasFoerdermixRequiredSumValidator.isValid(foerdermix, null),
                 is(false)
         );
 
-        foerdermix = new FoerdermixDto();
-        foerdermix.setAnteilFreifinanzierterGeschosswohnungsbau(null);
-        foerdermix.setAnteilGefoerderterMietwohnungsbau(BigDecimal.valueOf(99.99));
-        foerdermix.setAnteilMuenchenModell(null);
-        foerdermix.setAnteilPreisgedaempfterMietwohnungsbau(null);
-        foerdermix.setAnteilKonzeptionellerMietwohnungsbau(null);
-        foerdermix.setAnteilBaugemeinschaften(null);
-        foerdermix.setAnteilEinUndZweifamilienhaeuser(BigDecimal.valueOf(0));
-        assertThat(
-                this.hasFoerdermixRequiredSumValidator.isValid(foerdermix, null),
-                is(false)
-        );
 
-        foerdermix = new FoerdermixDto();
-        foerdermix.setAnteilFreifinanzierterGeschosswohnungsbau(null);
-        foerdermix.setAnteilGefoerderterMietwohnungsbau(null);
-        foerdermix.setAnteilMuenchenModell(null);
-        foerdermix.setAnteilPreisgedaempfterMietwohnungsbau(null);
-        foerdermix.setAnteilKonzeptionellerMietwohnungsbau(null);
-        foerdermix.setAnteilBaugemeinschaften(null);
-        foerdermix.setAnteilEinUndZweifamilienhaeuser(null);
+        foerderart.setAnteilProzent(null);
+        foerderart2.setAnteilProzent(BigDecimal.ZERO);
+        foerderart3.setAnteilProzent(BigDecimal.valueOf(99.99));
+
         assertThat(
                 this.hasFoerdermixRequiredSumValidator.isValid(foerdermix, null),
                 is(false)
