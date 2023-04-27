d367c6e0d3b7f3cdb38516c570cda206c691751d
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-04-27 16:35:29 +0200

    add vorertung data structure

--- src/main/java/de/muenchen/isi/infrastructure/entity/common/Flurstueck.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/common/Flurstueck.java
@@ -1,7 +1,6 @@
 package de.muenchen.isi.infrastructure.entity.common;
 
 import de.muenchen.isi.infrastructure.entity.BaseEntity;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import lombok.Data;
@@ -14,11 +13,4 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @EntityListeners(AuditingEntityListener.class)
-public class Stadtbezirk extends BaseEntity {
-
-    @Column(nullable = false)
-    private String nummer;
-
-    @Column(nullable = false)
-    private String name;
-}
+public class Flurstueck extends BaseEntity {}
--- src/main/java/de/muenchen/isi/infrastructure/entity/common/Gemarkung.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/common/Gemarkung.java
@@ -1,9 +1,13 @@
 package de.muenchen.isi.infrastructure.entity.common;
 
 import de.muenchen.isi.infrastructure.entity.BaseEntity;
-import javax.persistence.Column;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -14,11 +18,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @EntityListeners(AuditingEntityListener.class)
-public class Stadtbezirk extends BaseEntity {
+public class Gemarkung extends BaseEntity {
 
-    @Column(nullable = false)
-    private String nummer;
-
-    @Column(nullable = false)
-    private String name;
+    @OneToMany(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JoinColumn(name = "gemarkung_id", referencedColumnName = "id")
+    private Set<Flurstueck> flurstuecke;
 }
