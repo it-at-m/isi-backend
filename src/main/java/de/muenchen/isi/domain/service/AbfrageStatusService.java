fcb9a4187fd695bdc3be8f0fcf31355f5e38e1a1
tiago.kocevar <tiago.kocevar@muenchen.de>
2023-05-08 11:04:20 +0200

    🔥 Fix Errorhandling

--- src/main/java/de/muenchen/isi/domain/service/AbfrageStatusService.java
+++ src/main/java/de/muenchen/isi/domain/service/AbfrageStatusService.java
@@ -3,8 +3,6 @@ package de.muenchen.isi.domain.service;
 import de.muenchen.isi.configuration.StateMachineConfiguration;
 import de.muenchen.isi.domain.exception.AbfrageStatusNotAllowedException;
 import de.muenchen.isi.domain.exception.EntityNotFoundException;
-import de.muenchen.isi.domain.exception.FileHandlingFailedException;
-import de.muenchen.isi.domain.exception.FileHandlingWithS3FailedException;
 import de.muenchen.isi.domain.exception.OptimisticLockingException;
 import de.muenchen.isi.domain.exception.StateMachineTransitionFailedException;
 import de.muenchen.isi.domain.exception.UniqueViolationException;
@@ -231,9 +229,7 @@ public class AbfrageStatusService {
                             } catch (
                                 final EntityNotFoundException
                                 | OptimisticLockingException
-                                | UniqueViolationException
-                                | FileHandlingFailedException
-                                | FileHandlingWithS3FailedException exception
+                                | UniqueViolationException exception
                             ) {
                                 log.error(exception.getMessage(), exception);
                                 return null;
