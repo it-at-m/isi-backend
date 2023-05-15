5d2600fe36a7ccd0fe6050e59e84b781095c6e1c
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-05-15 13:19:07 +0200

    add db index for idealtypische baurate

--- src/main/java/de/muenchen/isi/infrastructure/entity/stammdaten/baurate/IdealtypischeBaurate.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/stammdaten/baurate/IdealtypischeBaurate.java
@@ -7,6 +7,8 @@ import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -14,8 +16,19 @@ import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
 @Entity
+@Table(
+    indexes = {
+        @Index(
+            name = "range_wohneinheiten_index",
+            columnList = "range_wohneinheiten_von DESC, range_wohneinheiten_bis_einschliesslich DESC"
+        ),
+        @Index(
+            name = "range_geschossflaeche_wohnen_index",
+            columnList = "range_geschossflaeche_wohnen_von DESC, range_geschossflaeche_wohnen_bis_einschliesslich DESC"
+        ),
+    }
+)
 @Data
-//@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "rangeWohneinheiten", "bezeichnungJahr" }) })
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @TypeDef(name = "json", typeClass = JsonType.class)
