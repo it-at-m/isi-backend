d6ec4192f90cbc34bc6b892ec8fe5b78769abaa4
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-05-15 13:05:16 +0200

    add attribute override

--- src/main/java/de/muenchen/isi/infrastructure/entity/stammdaten/baurate/IdealtypischeBaurate.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/stammdaten/baurate/IdealtypischeBaurate.java
@@ -3,6 +3,8 @@ package de.muenchen.isi.infrastructure.entity.stammdaten.baurate;
 import de.muenchen.isi.infrastructure.entity.BaseEntity;
 import io.hypersistence.utils.hibernate.type.json.JsonType;
 import java.util.List;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import lombok.Data;
@@ -13,14 +15,35 @@ import org.hibernate.annotations.TypeDef;
 
 @Entity
 @Data
-//@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "bezeichnung", "bezeichnungJahr" }) })
+//@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "rangeWohneinheiten", "bezeichnungJahr" }) })
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @TypeDef(name = "json", typeClass = JsonType.class)
 public class IdealtypischeBaurate extends BaseEntity {
 
+    @AttributeOverrides(
+        {
+            @AttributeOverride(name = "von", column = @Column(name = "range_wohneinheiten_von", nullable = false)),
+            @AttributeOverride(
+                name = "bisEinschliesslich",
+                column = @Column(name = "range_wohneinheiten_bis_einschliesslich", nullable = false)
+            ),
+        }
+    )
     private SelectionRange rangeWohneinheiten;
 
+    @AttributeOverrides(
+        {
+            @AttributeOverride(
+                name = "von",
+                column = @Column(name = "range_geschossflaeche_wohnen_von", nullable = false)
+            ),
+            @AttributeOverride(
+                name = "bisEinschliesslich",
+                column = @Column(name = "range_geschossflaeche_wohnen_bis_einschliesslich", nullable = false)
+            ),
+        }
+    )
     private SelectionRange rangeGeschossflaecheWohnen;
 
     @Type(type = "json")
