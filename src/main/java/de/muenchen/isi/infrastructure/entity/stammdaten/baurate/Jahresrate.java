0e1a83ff27af84337fd4d5e4fa519b6ebe27a874
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-05-15 12:49:23 +0200

    add Idealtypische Baurate

--- src/main/java/de/muenchen/isi/infrastructure/entity/stammdaten/baurate/Jahresrate.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/stammdaten/baurate/Jahresrate.java
@@ -1,11 +1,9 @@
 package de.muenchen.isi.infrastructure.entity.stammdaten.baurate;
 
 import java.math.BigDecimal;
-import javax.persistence.Embeddable;
 import lombok.Data;
 
 @Data
-@Embeddable
 public class Jahresrate {
 
     private Integer jahr;
