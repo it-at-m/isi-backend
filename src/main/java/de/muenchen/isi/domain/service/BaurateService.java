cadf8f2387539096339cb2bb92712ea8d54c05bf
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-05-16 14:07:07 +0200

    add rest endpoint for baurate determination

--- src/main/java/de/muenchen/isi/domain/service/BaurateService.java
+++ src/main/java/de/muenchen/isi/domain/service/BaurateService.java
@@ -1,33 +1,23 @@
 package de.muenchen.isi.domain.service;
 
 import de.muenchen.isi.domain.exception.EntityNotFoundException;
-import de.muenchen.isi.domain.exception.OptimisticLockingException;
-import de.muenchen.isi.domain.mapper.BaurateDomainMapper;
 import de.muenchen.isi.domain.model.BaurateModel;
 import de.muenchen.isi.infrastructure.entity.stammdaten.baurate.IdealtypischeBaurate;
-import de.muenchen.isi.infrastructure.repository.BaurateRepository;
 import de.muenchen.isi.infrastructure.repository.stammdaten.IdealtypischeBaurateRepository;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class BaurateService {
-
-    private final BaurateDomainMapper baurateDomainMapper;
-
-    private final BaurateRepository baurateRepository;
 
     private final IdealtypischeBaurateRepository idealtypischeBaurateRepository;
 
@@ -147,70 +137,5 @@ public class BaurateService {
         final BigDecimal rate
     ) {
         return gesamtwert.multiply(rate).setScale(0, RoundingMode.DOWN);
-    }
-
-    public List<BaurateModel> getBauraten() {
-        return this.baurateRepository.findAllByOrderByJahrDesc()
-            .map(this.baurateDomainMapper::entity2Model)
-            .collect(Collectors.toList());
-    }
-
-    /**
-     * Die Methode gibt ein {@link BaurateModel} identifiziert durch die ID zurück.
-     *
-     * @param id zum Identifizieren des {@link BaurateModel}.
-     * @return {@link BaurateModel}.
-     */
-    public BaurateModel getBaurateById(final UUID id) throws EntityNotFoundException {
-        final var optEntity = this.baurateRepository.findById(id);
-        final var entity = optEntity.orElseThrow(() -> {
-            final var message = "Baurate nicht gefunden.";
-            log.error(message);
-            return new EntityNotFoundException(message);
-        });
-        return this.baurateDomainMapper.entity2Model(entity);
-    }
-
-    /**
-     * Diese Methode speichert ein {@link BaurateModel}.
-     *
-     * @param baurate zum Speichern
-     * @return das gespeicherte {@link BaurateModel}
-     * @throws OptimisticLockingException falls in der Anwendung bereits eine neuere Version der Entität gespeichert ist
-     */
-    public BaurateModel saveBaurate(final BaurateModel baurate) throws OptimisticLockingException {
-        var entity = this.baurateDomainMapper.model2entity(baurate);
-        try {
-            entity = this.baurateRepository.saveAndFlush(entity);
-        } catch (final ObjectOptimisticLockingFailureException exception) {
-            final var message = "Die Daten wurden in der Zwischenzeit geändert. Bitte laden Sie die Daten neu";
-            throw new OptimisticLockingException(message, exception);
-        }
-        return this.baurateDomainMapper.entity2Model(entity);
-    }
-
-    /**
-     * Diese Methode updated ein {@link BaurateModel}.
-     *
-     * @param baurate zum Updaten
-     * @return das geupdatete {@link BaurateModel}
-     * @throws EntityNotFoundException falls die Abfrage identifiziert durch die {@link BaurateModel#getId()} nicht gefunden wird
-     * @throws OptimisticLockingException falls in der Anwendung bereits eine neuere Version der Entität gespeichert ist
-     */
-    public BaurateModel updateBaurate(final BaurateModel baurate)
-        throws EntityNotFoundException, OptimisticLockingException {
-        this.getBaurateById(baurate.getId());
-        return this.saveBaurate(baurate);
-    }
-
-    /**
-     * Diese Methode löscht ein {@link BaurateModel}.
-     *
-     * @param id zum Identifizieren des {@link BaurateModel}.
-     * @throws EntityNotFoundException falls die Abfrage identifiziert durch die {@link BaurateModel#getId()} nicht gefunden wird.
-     */
-    public void deleteBaurateById(final UUID id) throws EntityNotFoundException {
-        final var model = this.getBaurateById(id);
-        this.baurateRepository.deleteById(model.getId());
     }
 }
--- src/test/java/de/muenchen/isi/domain/service/BaurateServiceTest.java
+++ src/test/java/de/muenchen/isi/domain/service/BaurateServiceTest.java
@@ -1,21 +1,6 @@
 package de.muenchen.isi.domain.service;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
-import de.muenchen.isi.domain.exception.EntityNotFoundException;
-import de.muenchen.isi.domain.exception.OptimisticLockingException;
-import de.muenchen.isi.domain.mapper.BaurateDomainMapper;
-import de.muenchen.isi.domain.mapper.BaurateDomainMapperImpl;
-import de.muenchen.isi.domain.model.BaurateModel;
-import de.muenchen.isi.infrastructure.entity.Baurate;
-import de.muenchen.isi.infrastructure.repository.BaurateRepository;
 import de.muenchen.isi.infrastructure.repository.stammdaten.IdealtypischeBaurateRepository;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.stream.Stream;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,11 +14,6 @@ import org.mockito.quality.Strictness;
 @MockitoSettings(strictness = Strictness.LENIENT)
 class BaurateServiceTest {
 
-    private final BaurateDomainMapper baurateDomainMapper = new BaurateDomainMapperImpl();
-
-    @Mock
-    private BaurateRepository baurateRepository;
-
     @Mock
     private IdealtypischeBaurateRepository idealtypischeBaurateRepository;
 
@@ -41,102 +21,10 @@ class BaurateServiceTest {
 
     @BeforeEach
     public void beforeEach() {
-        this.baurateService =
-            new BaurateService(this.baurateDomainMapper, this.baurateRepository, this.idealtypischeBaurateRepository);
-        Mockito.reset(this.baurateRepository);
+        this.baurateService = new BaurateService(this.idealtypischeBaurateRepository);
+        Mockito.reset(this.idealtypischeBaurateRepository);
     }
 
     @Test
-    void getBauraten() {
-        final Baurate baurate1 = new Baurate();
-        baurate1.setId(UUID.randomUUID());
-        final Baurate baurate2 = new Baurate();
-        baurate2.setId(UUID.randomUUID());
-
-        Mockito.when(this.baurateRepository.findAllByOrderByJahrDesc()).thenReturn(Stream.of(baurate1, baurate2));
-
-        final List<BaurateModel> result = this.baurateService.getBauraten();
-
-        final BaurateModel model1 = new BaurateModel();
-        model1.setId(baurate1.getId());
-        final BaurateModel model2 = new BaurateModel();
-        model2.setId(baurate2.getId());
-
-        assertThat(result, is(List.of(model1, model2)));
-    }
-
-    @Test
-    void getBaurateById() throws EntityNotFoundException {
-        final UUID id = UUID.randomUUID();
-
-        Mockito.when(this.baurateRepository.findById(id)).thenReturn(Optional.of(new Baurate()));
-        final BaurateModel result = this.baurateService.getBaurateById(id);
-        assertThat(result, is(new BaurateModel()));
-        Mockito.verify(this.baurateRepository, Mockito.times(1)).findById(id);
-        Mockito.reset(this.baurateRepository);
-
-        Mockito.when(this.baurateRepository.findById(id)).thenReturn(Optional.empty());
-        Assertions.assertThrows(EntityNotFoundException.class, () -> this.baurateService.getBaurateById(id));
-        Mockito.verify(this.baurateRepository, Mockito.times(1)).findById(id);
-    }
-
-    @Test
-    void saveBaurate() throws OptimisticLockingException {
-        final BaurateModel model = new BaurateModel();
-        model.setId(null);
-
-        final Baurate entity = new Baurate();
-        entity.setId(model.getId());
-
-        final Baurate saveResult = new Baurate();
-        saveResult.setId(UUID.randomUUID());
-
-        Mockito.when(this.baurateRepository.saveAndFlush(entity)).thenReturn(saveResult);
-
-        final BaurateModel result = this.baurateService.saveBaurate(model);
-
-        final BaurateModel expected = new BaurateModel();
-        expected.setId(saveResult.getId());
-
-        assertThat(result, is(expected));
-
-        Mockito.verify(this.baurateRepository, Mockito.times(1)).saveAndFlush(entity);
-    }
-
-    @Test
-    void updateBaurate() throws EntityNotFoundException, OptimisticLockingException {
-        final BaurateModel model = new BaurateModel();
-        model.setId(UUID.randomUUID());
-
-        final Baurate entity = new Baurate();
-        entity.setId(model.getId());
-
-        Mockito.when(this.baurateRepository.findById(entity.getId())).thenReturn(Optional.of(entity));
-        Mockito.when(this.baurateRepository.saveAndFlush(entity)).thenReturn(entity);
-
-        final BaurateModel result = this.baurateService.updateBaurate(model);
-
-        final BaurateModel expected = new BaurateModel();
-        expected.setId(model.getId());
-
-        assertThat(result, is(model));
-
-        Mockito.verify(this.baurateRepository, Mockito.times(1)).findById(entity.getId());
-        Mockito.verify(this.baurateRepository, Mockito.times(1)).saveAndFlush(entity);
-    }
-
-    @Test
-    void deleteBaurateById() throws EntityNotFoundException {
-        final UUID id = UUID.randomUUID();
-
-        final Baurate entity = new Baurate();
-        entity.setId(id);
-
-        Mockito.when(this.baurateRepository.findById(entity.getId())).thenReturn(Optional.of(entity));
-
-        this.baurateService.deleteBaurateById(id);
-
-        Mockito.verify(this.baurateRepository, Mockito.times(1)).findById(entity.getId());
-        Mockito.verify(this.baurateRepository, Mockito.times(1)).deleteById(id);
-    }
+    void determineBauraten() {}
 }
