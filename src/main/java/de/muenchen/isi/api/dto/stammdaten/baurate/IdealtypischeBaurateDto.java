efdb4347e6564377396e39942a05cb00b563c7ff
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-05-22 09:12:51 +0200

    add validation to idealtpyische bauraten

--- src/main/java/de/muenchen/isi/api/dto/stammdaten/baurate/IdealtypischeBaurateDto.java
+++ src/main/java/de/muenchen/isi/api/dto/stammdaten/baurate/IdealtypischeBaurateDto.java
@@ -4,6 +4,9 @@ import de.muenchen.isi.api.dto.BaseEntityDto;
 import de.muenchen.isi.infrastructure.entity.enums.IdealtypischeBaurateTyp;
 import java.math.BigDecimal;
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -13,11 +16,15 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 public class IdealtypischeBaurateDto extends BaseEntityDto {
 
+    @NotNull
     private BigDecimal von;
 
+    @NotNull
     private BigDecimal bisExklusiv;
 
+    @NotNull
     private IdealtypischeBaurateTyp typ;
 
-    private List<JahresrateDto> jahresraten;
+    @NotEmpty
+    private List<@Valid JahresrateDto> jahresraten;
 }
--- src/main/java/de/muenchen/isi/api/dto/stammdaten/baurate/JahresrateDto.java
+++ src/main/java/de/muenchen/isi/api/dto/stammdaten/baurate/JahresrateDto.java
@@ -1,12 +1,17 @@
 package de.muenchen.isi.api.dto.stammdaten.baurate;
 
 import java.math.BigDecimal;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class JahresrateDto {
 
+    @Min(1)
     private Integer jahr;
 
+    @Min(0)
+    @Max(1)
     private BigDecimal rate;
 }
