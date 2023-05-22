00ada1aefe72e6ad1584c72c473761b1985abf7a
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-05-22 14:48:36 +0200

    add java doc

--- src/main/java/de/muenchen/isi/api/dto/stammdaten/baurate/JahresrateDto.java
+++ src/main/java/de/muenchen/isi/api/dto/stammdaten/baurate/JahresrateDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
@@ -13,7 +14,7 @@ public class JahresrateDto {
     private Integer jahr;
 
     @Schema(description = "Die Rate repräsentiert eine Prozentzahl großer den Wert 0 und kleiner gleich 1.")
-    @Min(0)
+    @Positive
     @Max(1)
     private BigDecimal rate;
 }
