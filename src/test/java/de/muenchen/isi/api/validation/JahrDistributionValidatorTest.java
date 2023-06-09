325e903a89bb960fc04f00a2b49dd7233d559f36
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-06-09 10:08:43 +0200

    add unit test

--- src/test/java/de/muenchen/isi/api/validation/JahrDistributionValidatorTest.java
+++ src/test/java/de/muenchen/isi/api/validation/JahrDistributionValidatorTest.java
@@ -45,6 +45,22 @@ class JahrDistributionValidatorTest {
         // --
 
         baugebiet = new BaugebietDto();
+        baugebiet.setRealisierungVon(2019);
+        baugebiet.setTechnical(false);
+
+        baurate1 = new BaurateDto();
+        baurate1.setJahr(2020);
+        baurate2 = new BaurateDto();
+        baurate2.setJahr(2021);
+        baurate3 = new BaurateDto();
+        baurate3.setJahr(2022);
+        baugebiet.setBauraten(List.of(baurate1, baurate2, baurate3));
+
+        assertThat(this.jahrDistributionValidator.isValid(baugebiet, null), is(true));
+
+        // --
+
+        baugebiet = new BaugebietDto();
         baugebiet.setRealisierungVon(2021);
         baugebiet.setTechnical(false);
 
