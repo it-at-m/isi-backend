1911a6117653d52558fe05080d11bc2b27150ece
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-06-19 11:15:37 +0200

    add order by at relation abfragevariante -> BedarfsmeldungFachreferate

--- src/main/java/de/muenchen/isi/infrastructure/entity/AbfragevarianteSachbearbeitung.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/AbfragevarianteSachbearbeitung.java
@@ -7,7 +7,15 @@ package de.muenchen.isi.infrastructure.entity;
 import de.muenchen.isi.infrastructure.entity.enums.lookup.SobonOrientierungswertJahr;
 import java.math.BigDecimal;
 import java.util.List;
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import lombok.Data;
 import lombok.ToString;
 
@@ -28,5 +36,6 @@ public class AbfragevarianteSachbearbeitung {
 
     @OneToMany(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER, orphanRemoval = true)
     @JoinColumn(name = "abfragevariante_id", referencedColumnName = "id")
+    @OrderBy("createdDateTime asc")
     private List<BedarfsmeldungFachreferate> bedarfsmeldungFachreferate;
 }
