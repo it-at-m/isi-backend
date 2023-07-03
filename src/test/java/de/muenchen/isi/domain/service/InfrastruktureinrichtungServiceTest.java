e0abba97554c154a05ec6082eca83a0e2a6acdf4
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-07-03 14:55:28 +0200

    add unit test

--- src/test/java/de/muenchen/isi/domain/service/InfrastruktureinrichtungServiceTest.java
+++ src/test/java/de/muenchen/isi/domain/service/InfrastruktureinrichtungServiceTest.java
@@ -2,15 +2,19 @@ package de.muenchen.isi.domain.service;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 import de.muenchen.isi.IsiBackendApplication;
 import de.muenchen.isi.TestConstants;
 import de.muenchen.isi.domain.exception.EntityIsReferencedException;
 import de.muenchen.isi.domain.exception.EntityNotFoundException;
+import de.muenchen.isi.domain.exception.OptimisticLockingException;
 import de.muenchen.isi.domain.model.BauvorhabenModel;
 import de.muenchen.isi.domain.model.infrastruktureinrichtung.InfrastruktureinrichtungModel;
 import de.muenchen.isi.domain.model.infrastruktureinrichtung.KindergartenModel;
 import de.muenchen.isi.domain.model.infrastruktureinrichtung.KinderkrippeModel;
+import de.muenchen.isi.domain.model.infrastruktureinrichtung.MittelschuleModel;
+import de.muenchen.isi.domain.model.infrastruktureinrichtung.SchuleModel;
 import de.muenchen.isi.infrastructure.entity.enums.lookup.StatusInfrastruktureinrichtung;
 import de.muenchen.isi.infrastructure.entity.infrastruktureinrichtung.Grundschule;
 import de.muenchen.isi.infrastructure.entity.infrastruktureinrichtung.Kindergarten;
@@ -87,6 +91,94 @@ class InfrastruktureinrichtungServiceTest {
         Assertions.assertThrows(
             EntityNotFoundException.class,
             () -> this.infrastruktureinrichtungService.getInfrastruktureinrichtungById(UUID.randomUUID())
+        );
+    }
+
+    @Test
+    void saveInfrastruktureinrichtung() throws OptimisticLockingException {
+        MittelschuleModel mittelschule = new MittelschuleModel();
+        mittelschule.setNameEinrichtung("Mittelschule");
+        mittelschule.setStatus(StatusInfrastruktureinrichtung.UNGESICHERTE_PLANUNG_TF_KITA_STANDORT);
+        mittelschule.setSchule(new SchuleModel());
+        mittelschule.getSchule().setAnzahlKlassen(3);
+        mittelschule.getSchule().setAnzahlPlaetze(30);
+
+        final var savedMittelschule = this.infrastruktureinrichtungService.saveInfrastruktureinrichtung(mittelschule);
+
+        assertThat(savedMittelschule.getId(), is(notNullValue()));
+        assertThat(savedMittelschule.getVersion(), is(0L));
+        assertThat(savedMittelschule.getNameEinrichtung(), is(mittelschule.getNameEinrichtung()));
+        assertThat(
+            savedMittelschule.getInfrastruktureinrichtungTyp(),
+            is(mittelschule.getInfrastruktureinrichtungTyp())
+        );
+        assertThat(savedMittelschule.getClass(), is(mittelschule.getClass()));
+        assertThat(
+            ((MittelschuleModel) savedMittelschule).getSchule().getAnzahlKlassen(),
+            is(((MittelschuleModel) mittelschule).getSchule().getAnzahlKlassen())
+        );
+        assertThat(
+            ((MittelschuleModel) savedMittelschule).getSchule().getAnzahlPlaetze(),
+            is(((MittelschuleModel) mittelschule).getSchule().getAnzahlPlaetze())
+        );
+    }
+
+    @Test
+    void updateInfrastruktureinrichtung() throws OptimisticLockingException, EntityNotFoundException {
+        MittelschuleModel mittelschule = new MittelschuleModel();
+        mittelschule.setNameEinrichtung("Mittelschule");
+        mittelschule.setStatus(StatusInfrastruktureinrichtung.UNGESICHERTE_PLANUNG_TF_KITA_STANDORT);
+        mittelschule.setSchule(new SchuleModel());
+        mittelschule.getSchule().setAnzahlKlassen(3);
+        mittelschule.getSchule().setAnzahlPlaetze(30);
+
+        final var savedMittelschule = this.infrastruktureinrichtungService.saveInfrastruktureinrichtung(mittelschule);
+        assertThat(savedMittelschule.getVersion(), is(0L));
+
+        savedMittelschule.setNameEinrichtung("Mittelschule XXX");
+
+        final var updatedMittelschule =
+            this.infrastruktureinrichtungService.updateInfrastruktureinrichtung(savedMittelschule);
+
+        assertThat(updatedMittelschule.getId(), is(savedMittelschule.getId()));
+        assertThat(updatedMittelschule.getVersion(), is(1L));
+        assertThat(updatedMittelschule.getNameEinrichtung(), is("Mittelschule XXX"));
+        assertThat(
+            updatedMittelschule.getInfrastruktureinrichtungTyp(),
+            is(savedMittelschule.getInfrastruktureinrichtungTyp())
+        );
+        assertThat(updatedMittelschule.getClass(), is(savedMittelschule.getClass()));
+        assertThat(
+            ((MittelschuleModel) updatedMittelschule).getSchule().getAnzahlKlassen(),
+            is(((MittelschuleModel) savedMittelschule).getSchule().getAnzahlKlassen())
+        );
+        assertThat(
+            ((MittelschuleModel) updatedMittelschule).getSchule().getAnzahlPlaetze(),
+            is(((MittelschuleModel) savedMittelschule).getSchule().getAnzahlPlaetze())
+        );
+
+        updatedMittelschule.setVersion(0L);
+        Assertions.assertThrows(
+            OptimisticLockingException.class,
+            () -> this.infrastruktureinrichtungService.updateInfrastruktureinrichtung(updatedMittelschule)
+        );
+
+        updatedMittelschule.setId(UUID.randomUUID());
+        Assertions.assertThrows(
+            EntityNotFoundException.class,
+            () -> this.infrastruktureinrichtungService.updateInfrastruktureinrichtung(updatedMittelschule)
+        );
+    }
+
+    @Test
+    void deleteInfrastruktureinrichtungById() throws EntityIsReferencedException, EntityNotFoundException {
+        final var entity = infrastruktureinrichtungRepository.findAll().get(0);
+
+        this.infrastruktureinrichtungService.deleteInfrastruktureinrichtungById(entity.getId());
+
+        Assertions.assertThrows(
+            EntityNotFoundException.class,
+            () -> this.infrastruktureinrichtungService.deleteInfrastruktureinrichtungById(entity.getId())
         );
     }
 
