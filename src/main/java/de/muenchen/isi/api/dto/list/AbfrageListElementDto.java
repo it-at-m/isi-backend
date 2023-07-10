b5b0637fd749e68daf3b7103a3f12008fdde9153
mo996 <mdaeeboul@gmail.com>
2023-07-10 11:10:58 +0200

    zwischenspeichern

--- src/main/java/de/muenchen/isi/api/dto/list/AbfrageListElementDto.java
+++ src/main/java/de/muenchen/isi/api/dto/list/AbfrageListElementDto.java
@@ -1,10 +1,12 @@
 package de.muenchen.isi.api.dto.list;
 
+import de.muenchen.isi.api.dto.common.StadtbezirkDto;
 import de.muenchen.isi.domain.model.enums.AbfrageTyp;
 import de.muenchen.isi.infrastructure.entity.enums.lookup.SobonVerfahrensgrundsaetzeJahr;
 import de.muenchen.isi.infrastructure.entity.enums.lookup.StandVorhaben;
 import de.muenchen.isi.infrastructure.entity.enums.lookup.StatusAbfrage;
 import java.time.LocalDate;
+import java.util.Set;
 import java.util.UUID;
 import javax.validation.constraints.Size;
 import lombok.Data;
@@ -17,7 +19,7 @@ public class AbfrageListElementDto {
     @Size(max = 70, message = "Es sind maximal {max} Zeichen erlaubt")
     private String nameAbfrage;
 
-    private StandVorhaben standVorhaben;
+    private Set<StadtbezirkDto> stadtbezirke;
 
     private StatusAbfrage statusAbfrage;
 
