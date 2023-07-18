e85224605ead87afbd76155ed55f375d50a81349
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-07-18 11:23:54 +0200

    add filter to analyzer

--- src/main/java/de/muenchen/isi/domain/service/search/SucheService.java
+++ src/main/java/de/muenchen/isi/domain/service/search/SucheService.java
@@ -1,5 +1,7 @@
 package de.muenchen.isi.domain.service.search;
 
+import de.muenchen.isi.infrastructure.entity.Abfrage;
+import de.muenchen.isi.infrastructure.entity.BaseEntity;
 import de.muenchen.isi.infrastructure.entity.Bauvorhaben;
 import de.muenchen.isi.infrastructure.entity.Infrastrukturabfrage;
 import de.muenchen.isi.infrastructure.entity.infrastruktureinrichtung.Infrastruktureinrichtung;
@@ -7,12 +9,13 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.collections4.SetUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.hibernate.search.engine.search.common.BooleanOperator;
@@ -29,15 +32,16 @@ public class SucheService {
 
     private String[] searchableAttributes;
 
-    public void searchForEntities(final String searchQuery) {
+    public List<? extends BaseEntity> searchForEntities(final String searchQuery) {
         final var adaptedSearchQuery = this.createAdaptedSearchQuery(searchQuery);
 
         final var searchSession = Search.session(entityManager.getEntityManagerFactory().createEntityManager());
 
-        searchSession
+        return searchSession
             .search(List.of(Infrastrukturabfrage.class, Bauvorhaben.class, Infrastruktureinrichtung.class))
             .where(f ->
                 f
+                    // https://docs.jboss.org/hibernate/stable/search/reference/en-US/html_single/#search-dsl-predicate-simple-query-string
                     .simpleQueryString()
                     .fields(searchableAttributes)
                     .matching(adaptedSearchQuery)
@@ -52,7 +56,9 @@ public class SucheService {
         adaptedSearchQuery =
             Arrays
                 .stream(singleQueryWords)
+                // Abfügen eines Wildcardprefix
                 .map(queryWord -> queryWord + "*")
+                // Trennen der Wildcard-Suchwörter mit Leerzeichen
                 .collect(Collectors.joining(StringUtils.SPACE));
         log.debug("Die erstellte Suchquery: {}", adaptedSearchQuery);
         return adaptedSearchQuery;
@@ -60,21 +66,21 @@ public class SucheService {
 
     @PostConstruct
     protected void setNamesOfSearchableAttributes() {
-        List<Class<? extends Annotation>> searchIndexAnnotation = List.of(FullTextField.class);
+        Set<Class<? extends Annotation>> searchIndexAnnotation = Set.of(FullTextField.class);
 
         final var fieldNamesAbfrage = searchIndexAnnotation
             .stream()
-            .flatMap(annotation ->
-                FieldUtils.getFieldsListWithAnnotation(Infrastruktureinrichtung.class, annotation).stream()
-            )
+            .flatMap(annotation -> FieldUtils.getFieldsListWithAnnotation(Abfrage.class, annotation).stream())
             .map(Field::getName)
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
+        log.debug("Die Namen der suchbaren Attribute in der Abfrage: {}", fieldNamesAbfrage);
 
         final var fieldNamesBauvorhaben = searchIndexAnnotation
             .stream()
             .flatMap(annotation -> FieldUtils.getFieldsListWithAnnotation(Bauvorhaben.class, annotation).stream())
             .map(Field::getName)
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
+        log.debug("Die Namen der suchbaren Attribute in der Bauvorhaben: {}", fieldNamesBauvorhaben);
 
         final var fieldNamesEinrichtung = searchIndexAnnotation
             .stream()
@@ -82,12 +88,13 @@ public class SucheService {
                 FieldUtils.getFieldsListWithAnnotation(Infrastruktureinrichtung.class, annotation).stream()
             )
             .map(Field::getName)
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
+        log.debug("Die Namen der suchbaren Attribute in der Infrastruktureinrichtung: {}", fieldNamesEinrichtung);
 
-        searchableAttributes =
-            ListUtils
-                .union(ListUtils.union(fieldNamesAbfrage, fieldNamesBauvorhaben), fieldNamesEinrichtung)
+        this.searchableAttributes =
+            SetUtils
+                .union(SetUtils.union(fieldNamesAbfrage, fieldNamesBauvorhaben), fieldNamesEinrichtung)
                 .toArray(String[]::new);
-        log.debug("Die Namen der suchbaren Attribute: {}", searchableAttributes);
+        log.debug("Die Namen aller suchbaren Attribute: {}", Arrays.toString(this.searchableAttributes));
     }
 }
