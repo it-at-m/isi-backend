363426a9979c453c771aa9b290442b863f3ecbdf
tiago.kocevar <tiago.kocevar@muenchen.de>
2023-09-14 15:19:39 +0200

    Validierung und Testcases erweitert

--- src/main/java/de/muenchen/isi/api/validation/TechnicalAttributesValidator.java
+++ src/main/java/de/muenchen/isi/api/validation/TechnicalAttributesValidator.java
@@ -1,12 +1,12 @@
 package de.muenchen.isi.api.validation;
 
-import de.muenchen.isi.api.dto.BaugebietDto;
 import de.muenchen.isi.api.dto.abfrageAbfrageerstellungAngelegt.AbfragevarianteAngelegtDto;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class TechnicalAttributesValidator
-    implements ConstraintValidator<TechnicalAttributesValid, AbfragevarianteAngelegtDto> {
+        implements ConstraintValidator<TechnicalAttributesValid, AbfragevarianteAngelegtDto> {
 
     @Override
     public boolean isValid(AbfragevarianteAngelegtDto abfragevarianteDto, ConstraintValidatorContext context) {
@@ -14,72 +14,77 @@ public class TechnicalAttributesValidator
             return true; // Wenn die DTO null ist, überspringe die Validierung
         }
 
-        return (
-            isValidForOption1(abfragevarianteDto) ||
-            isValidForOption2(abfragevarianteDto) ||
-            isValidForOption3(abfragevarianteDto)
-        );
+        return isValidForOption1(abfragevarianteDto) ||
+                isValidForOption2(abfragevarianteDto) ||
+                isValidForOption3(abfragevarianteDto);
     }
 
-    // Möglichkeit 1: Die Bauraten sind über fachliche Bauabschnitte und Baugebiete an die Abfragevariante angebunden
-    // Abfragevariante -> Bauabschnitte (nicht technisch) -> Baugebiete (nicht technisch) -> Bauraten
-    private boolean isValidForOption1(final AbfragevarianteAngelegtDto abfragevarianteDto) {
+    private boolean isValidForOption1(AbfragevarianteAngelegtDto abfragevarianteDto) {
         return (
-            abfragevarianteDto.getBauabschnitte() != null &&
-            abfragevarianteDto
-                .getBauabschnitte()
-                .stream()
-                .anyMatch(bauabschnitt ->
-                    !bauabschnitt.getTechnical() &&
-                    bauabschnitt.getBaugebiete() != null &&
-                    bauabschnitt
-                        .getBaugebiete()
-                        .stream()
-                        .anyMatch(baugebiet ->
-                            !baugebiet.getTechnical() &&
-                            baugebiet.getBauraten() != null &&
-                            !baugebiet.getBauraten().isEmpty()
-                        )
-                )
-        );
+                abfragevarianteDto.getBauabschnitte() != null &&
+                        abfragevarianteDto
+                                .getBauabschnitte()
+                                .stream()
+                                .anyMatch(bauabschnitt ->
+                                        !bauabschnitt.getTechnical() &&
+                                                bauabschnitt.getBaugebiete() != null &&
+                                                bauabschnitt
+                                                        .getBaugebiete()
+                                                        .stream()
+                                                        .anyMatch(baugebiet ->
+                                                                !baugebiet.getTechnical() &&
+                                                                        baugebiet.getBauraten() != null &&
+                                                                        !baugebiet.getBauraten().isEmpty()
+                                                        )
+                                )
+        ) && hasBauraten(abfragevarianteDto);
     }
 
-    // Möglichkeit 2: Die Bauraten sind über fachliche Baugebiete an die Abfragevariante angebunden
-    // Abfragevariante -> Bauabschnitt (technisch) -> Baugebiete (nicht technisch) -> Bauraten
-    private boolean isValidForOption2(final AbfragevarianteAngelegtDto abfragevarianteDto) {
+    private boolean isValidForOption2(AbfragevarianteAngelegtDto abfragevarianteDto) {
         return (
-            abfragevarianteDto.getBauabschnitte() != null &&
-            abfragevarianteDto
-                .getBauabschnitte()
-                .stream()
-                .anyMatch(bauabschnitt ->
-                    bauabschnitt.getTechnical() &&
-                    bauabschnitt.getBaugebiete() != null &&
-                    bauabschnitt
-                        .getBaugebiete()
-                        .stream()
-                        .anyMatch(baugebiet ->
-                            !baugebiet.getTechnical() &&
-                            baugebiet.getBauraten() != null &&
-                            !baugebiet.getBauraten().isEmpty()
-                        )
-                )
-        );
+                abfragevarianteDto.getBauabschnitte() != null &&
+                        abfragevarianteDto
+                                .getBauabschnitte()
+                                .stream()
+                                .anyMatch(bauabschnitt ->
+                                        bauabschnitt.getTechnical() &&
+                                                bauabschnitt.getBaugebiete() != null &&
+                                                bauabschnitt
+                                                        .getBaugebiete()
+                                                        .stream()
+                                                        .anyMatch(baugebiet ->
+                                                                !baugebiet.getTechnical() &&
+                                                                        baugebiet.getBauraten() != null &&
+                                                                        !baugebiet.getBauraten().isEmpty()
+                                                        )
+                                )
+        ) && hasBauraten(abfragevarianteDto);
     }
 
-    // Möglichkeit 3: Die Bauraten sind direkt an die Abfragevariante angebunden (nur erlaubt, wenn Bauabschnitte und Baugebiete technisch sind)
-    // Abfragevariante -> Bauabschnitt (technisch) -> Baugebiet (technisch) -> Bauraten
-    private boolean isValidForOption3(final AbfragevarianteAngelegtDto abfragevarianteDto) {
+    private boolean isValidForOption3(AbfragevarianteAngelegtDto abfragevarianteDto) {
         return (
-            abfragevarianteDto.getBauabschnitte() != null &&
-            abfragevarianteDto
-                .getBauabschnitte()
-                .stream()
-                .allMatch(bauabschnitt ->
-                    bauabschnitt.getTechnical() &&
-                    bauabschnitt.getBaugebiete() != null &&
-                    bauabschnitt.getBaugebiete().stream().allMatch(BaugebietDto::getTechnical)
-                )
-        );
+                abfragevarianteDto.getBauabschnitte() != null &&
+                        abfragevarianteDto
+                                .getBauabschnitte()
+                                .stream()
+                                .allMatch(bauabschnitt ->
+                                        bauabschnitt.getTechnical() &&
+                                                bauabschnitt.getBaugebiete() != null &&
+                                                bauabschnitt.getBaugebiete().stream().allMatch(baugebiet ->
+                                                        baugebiet.getTechnical() &&
+                                                                baugebiet.getBauraten() != null &&
+                                                                !baugebiet.getBauraten().isEmpty()
+                                                )
+                                )
+        ) && hasBauraten(abfragevarianteDto);
+    }
+
+    private boolean hasBauraten(AbfragevarianteAngelegtDto abfragevarianteDto) {
+        return abfragevarianteDto.getBauabschnitte() != null &&
+                abfragevarianteDto.getBauabschnitte().stream()
+                        .flatMap(bauabschnitt -> bauabschnitt.getBaugebiete().stream())
+                        .flatMap(baugebiet -> baugebiet.getBauraten().stream())
+                        .findAny()
+                        .isPresent();
     }
 }
--- src/test/java/de/muenchen/isi/api/validation/TechnicalAttributesValidatorTest.java
+++ src/test/java/de/muenchen/isi/api/validation/TechnicalAttributesValidatorTest.java
@@ -1,21 +1,22 @@
 package de.muenchen.isi.api.validation;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 import de.muenchen.isi.api.dto.BauabschnittDto;
 import de.muenchen.isi.api.dto.BaugebietDto;
 import de.muenchen.isi.api.dto.BaurateDto;
 import de.muenchen.isi.api.dto.abfrageAbfrageerstellungAngelegt.AbfragevarianteAngelegtDto;
-import java.util.List;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class TechnicalAttributesValidatorTest {
 
     private final TechnicalAttributesValidator technicalAttributesValidator = new TechnicalAttributesValidator();
 
     @Test
-    void isValidTechnicalBauabschnittAndTechnicalBaugebiet() {
+    void isValidTechnicalBauabschnittAndTechnicalBaugebietWithBaurate() {
         final var baurateDto = new BaurateDto();
         final var baugebietDto = new BaugebietDto();
         baugebietDto.setTechnical(true);
@@ -30,7 +31,7 @@ public class TechnicalAttributesValidatorTest {
     }
 
     @Test
-    void isValidTechnicalBauabschnittAndNotTechnicalBaugebiet() {
+    void isValidTechnicalBauabschnittAndNotTechnicalBaugebietWithBaurate() {
         final var baurateDto = new BaurateDto();
         final var baugebietDto = new BaugebietDto();
         baugebietDto.setTechnical(false);
@@ -45,7 +46,7 @@ public class TechnicalAttributesValidatorTest {
     }
 
     @Test
-    void isValidNotTechnicalBauabschnittAndNotTechnicalBaugebiet() {
+    void isValidNotTechnicalBauabschnittAndNotTechnicalBaugebietWithBaurate() {
         final var baurateDto = new BaurateDto();
         final var baugebietDto = new BaugebietDto();
         baugebietDto.setTechnical(false);
@@ -60,7 +61,7 @@ public class TechnicalAttributesValidatorTest {
     }
 
     @Test
-    void isNotValidNotTechnicalBauabschnittAndTechnicalBaugebiet() {
+    void isNotValidNotTechnicalBauabschnittAndTechnicalBaugebietWithBaurate() {
         final var baurateDto = new BaurateDto();
         final var baugebietDto = new BaugebietDto();
         baugebietDto.setTechnical(true);
@@ -71,6 +72,58 @@ public class TechnicalAttributesValidatorTest {
         final var abfragevarianteDto = new AbfragevarianteAngelegtDto();
         abfragevarianteDto.setBauabschnitte(List.of(bauabschnittDto));
 
+        assertThat(this.technicalAttributesValidator.isValid(abfragevarianteDto, null), is(false));
+    }
+
+    @Test
+    void isNotValidOnlyTechnicalBauabschnitte() {
+        final var bauabschnittDto = new BauabschnittDto();
+        bauabschnittDto.setTechnical(true);
+        final var abfragevarianteDto = new AbfragevarianteAngelegtDto();
+        abfragevarianteDto.setBauabschnitte(List.of(bauabschnittDto));
+
+        assertThat(this.technicalAttributesValidator.isValid(abfragevarianteDto, null), is(false));
+    }
+
+    @Test
+    void isNotValidOnlyNonTechnicalBauabschnitte() {
+        final var bauabschnittDto = new BauabschnittDto();
+        bauabschnittDto.setTechnical(false);
+        final var abfragevarianteDto = new AbfragevarianteAngelegtDto();
+        abfragevarianteDto.setBauabschnitte(List.of(bauabschnittDto));
+
+        assertThat(this.technicalAttributesValidator.isValid(abfragevarianteDto, null), is(false));
+    }
+
+    @Test
+    void isNotValidOnlyNonTechnicalBauabschnitteAndNonTechnicalBaugebieteWithoutBaurate() {
+        final var baugebietDto = new BaugebietDto();
+        baugebietDto.setTechnical(false);
+        final var bauabschnittDto = new BauabschnittDto();
+        bauabschnittDto.setTechnical(false);
+        bauabschnittDto.setBaugebiete(List.of(baugebietDto));
+        final var abfragevarianteDto = new AbfragevarianteAngelegtDto();
+        abfragevarianteDto.setBauabschnitte(List.of(bauabschnittDto));
+
+        assertThat(this.technicalAttributesValidator.isValid(abfragevarianteDto, null), is(false));
+    }
+
+    @Test
+    void isNotValidOnlyTechnicalBauabschnitteAndTechnicalBaugebieteWithoutBaurate() {
+        final var baugebietDto = new BaugebietDto();
+        baugebietDto.setTechnical(true);
+        final var bauabschnittDto = new BauabschnittDto();
+        bauabschnittDto.setTechnical(true);
+        bauabschnittDto.setBaugebiete(List.of(baugebietDto));
+        final var abfragevarianteDto = new AbfragevarianteAngelegtDto();
+        abfragevarianteDto.setBauabschnitte(List.of(bauabschnittDto));
+
+        assertThat(this.technicalAttributesValidator.isValid(abfragevarianteDto, null), is(false));
+    }
+
+    @Test
+    void isValidNoBauratenInAbfragevariante() {
+        final var abfragevarianteDto = new AbfragevarianteAngelegtDto();
         assertThat(this.technicalAttributesValidator.isValid(abfragevarianteDto, null), is(false));
     }
 }
