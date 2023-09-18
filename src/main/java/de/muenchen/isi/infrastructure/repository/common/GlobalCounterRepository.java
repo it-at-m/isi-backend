28033e495e24dacd92763503cef7757fdf16ae3c
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-09-18 11:06:09 +0200

    add repo method

--- src/main/java/de/muenchen/isi/infrastructure/repository/common/GlobalCounterRepository.java
+++ src/main/java/de/muenchen/isi/infrastructure/repository/common/GlobalCounterRepository.java
@@ -1,7 +1,11 @@
 package de.muenchen.isi.infrastructure.repository.common;
 
 import de.muenchen.isi.infrastructure.entity.common.GlobalCounter;
+import de.muenchen.isi.infrastructure.entity.enums.CounterType;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GlobalCounterRepository extends JpaRepository<GlobalCounter, UUID> {}
+public interface GlobalCounterRepository extends JpaRepository<GlobalCounter, UUID> {
+    Optional<GlobalCounter> findByCounterType(final CounterType counterType);
+}
