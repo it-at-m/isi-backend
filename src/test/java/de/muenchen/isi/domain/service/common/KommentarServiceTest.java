2b72e74efae4fc00b7dcaecb74232b0d19249c97
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-09-26 15:31:03 +0200

    add unit test

--- src/test/java/de/muenchen/isi/domain/service/common/KommentarServiceTest.java
+++ src/test/java/de/muenchen/isi/domain/service/common/KommentarServiceTest.java
@@ -2,12 +2,15 @@ package de.muenchen.isi.domain.service.common;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import de.muenchen.isi.domain.exception.EntityNotFoundException;
 import de.muenchen.isi.domain.mapper.KommentarDomainMapperImpl;
 import de.muenchen.isi.domain.model.common.KommentarModel;
 import de.muenchen.isi.infrastructure.entity.common.Kommentar;
 import de.muenchen.isi.infrastructure.repository.common.KommentarRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
@@ -116,5 +119,32 @@ class KommentarServiceTest {
         Mockito
             .verify(this.kommentarRepository, Mockito.times(1))
             .findAllByInfrastruktureinrichtungOrderByCreatedDateTimeDesc(uuidInfrastruktureinrichtung);
+    }
+
+    @Test
+    void getKommentarById() throws EntityNotFoundException {
+        final var uuidInfrastruktureinrichtung = UUID.randomUUID();
+        final var kommentar1 = new Kommentar();
+        kommentar1.setId(UUID.randomUUID());
+        kommentar1.setDatum("datum 1");
+        kommentar1.setText("text 1");
+        kommentar1.setBauvorhaben(uuidInfrastruktureinrichtung);
+
+        Mockito.when(this.kommentarRepository.findById(kommentar1.getId())).thenReturn(Optional.of(kommentar1));
+        final KommentarModel result = this.kommentarService.getKommentarById(kommentar1.getId());
+
+        final var kommentar1Model = new KommentarModel();
+        kommentar1Model.setId(kommentar1.getId());
+        kommentar1Model.setDatum("datum 1");
+        kommentar1Model.setText("text 1");
+        kommentar1Model.setBauvorhaben(uuidInfrastruktureinrichtung);
+        assertThat(kommentar1Model, is(result));
+
+        Mockito.verify(this.kommentarRepository, Mockito.times(1)).findById(kommentar1.getId());
+        Mockito.reset(this.kommentarRepository);
+
+        Mockito.when(this.kommentarRepository.findById(kommentar1.getId())).thenReturn(Optional.empty());
+        assertThrows(EntityNotFoundException.class, () -> this.kommentarService.getKommentarById(kommentar1.getId()));
+        Mockito.verify(this.kommentarRepository, Mockito.times(1)).findById(kommentar1.getId());
     }
 }
