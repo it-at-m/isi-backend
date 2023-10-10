63831180a1f2198b0fdaa2e2b7f1daba3de399f7
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-10-10 12:16:58 +0200

    add controller

---
+++ src/main/java/de/muenchen/isi/api/controller/BauleitplanverfahrenController.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2022
+ */
+package de.muenchen.isi.api.controller;
+
+import de.muenchen.isi.api.dto.BauleitplanverfahrenDto;
+import de.muenchen.isi.api.dto.error.InformationResponseDto;
+import de.muenchen.isi.api.mapper.AbfrageApiMapper;
+import de.muenchen.isi.domain.exception.EntityNotFoundException;
+import de.muenchen.isi.domain.mapper.AbfrageDomainMapper;
+import de.muenchen.isi.domain.service.AbfrageService;
+import de.muenchen.isi.domain.service.BauvorhabenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
+import javax.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/bauleitplanverfahren")
+@Tag(name = "Bauleitplanverfahren", description = "API zum interagieren mit Abfragen vom Typ Bauleitplanverfahren")
+@Validated
+public class BauleitplanverfahrenController {
+
+    private final AbfrageService abfrageService;
+
+    private final BauvorhabenService bauvorhabenService;
+
+    private final AbfrageApiMapper abfrageApiMapper;
+
+    private final AbfrageDomainMapper abfrageDomainMapper;
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    @Operation(summary = "Lesen einer Abfrage vom Typ Bauleitplanverfahren.")
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(
+                responseCode = "404",
+                description = "NOT FOUND -> Abfrage mit dieser ID nicht vorhanden.",
+                content = @Content(schema = @Schema(implementation = InformationResponseDto.class))
+            ),
+        }
+    )
+    @PreAuthorize("hasAuthority(T(de.muenchen.isi.security.AuthoritiesEnum).ISI_BACKEND_READ_ABFRAGE.name())")
+    public ResponseEntity<BauleitplanverfahrenDto> getBauleitplanverfahren(@PathVariable @NotNull final UUID id)
+        throws EntityNotFoundException {
+        return ResponseEntity.ok(new BauleitplanverfahrenDto());
+    }
+}
