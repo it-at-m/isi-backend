c00a6747f4ee08593566de75967b870fb54e7fb4
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-10-26 17:14:06 +0200

    validator wohnungsnahe plaetze

--- src/main/java/de/muenchen/isi/api/validation/WohnungsnahePlaetzeValidator.java
+++ src/main/java/de/muenchen/isi/api/validation/WohnungsnahePlaetzeValidator.java
@@ -4,7 +4,6 @@ import de.muenchen.isi.api.dto.infrastruktureinrichtung.HausFuerKinderDto;
 import de.muenchen.isi.api.dto.infrastruktureinrichtung.InfrastruktureinrichtungDto;
 import de.muenchen.isi.api.dto.infrastruktureinrichtung.KindergartenDto;
 import de.muenchen.isi.api.dto.infrastruktureinrichtung.KinderkrippeDto;
-import de.muenchen.isi.infrastructure.entity.enums.lookup.StatusInfrastruktureinrichtung;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import lombok.NoArgsConstructor;
@@ -17,12 +16,12 @@ public class WohnungsnahePlaetzeValidator
     implements ConstraintValidator<WohnungsnahePlaetzeValid, InfrastruktureinrichtungDto> {
 
     /**
-     * Prüft, ob das Feld einrichtungstraeger nicht null ist.
-     * null ist jedoch erlaubt, wenn {@link InfrastruktureinrichtungDto#getStatus()} weder {@link StatusInfrastruktureinrichtung#BESTAND} noch {@link StatusInfrastruktureinrichtung#GESICHERTE_PLANUNG_ERW_PLAETZE_BEST_EINR} ist.
+     * Prüft, ob die Anzahl der wohungsnahen Plätze die Anzahl der insgesamt zur Verfügung stehenden Plätze der Kinderkrippe,
+     * des Kindergartens und des Horts übersteigt.
      *
      * @param value   {@link InfrastruktureinrichtungDto} zum Validieren.
      * @param context in welchem die Validierung stattfindet.
-     * @return true, falls das Fertigstellungsjahr einen gültigen Wert hat, andernfalls false.
+     * @return true, falls die Anzahl der wohungsnahen Plätze die Gesamtanzahl der Platze nicht übersteigt, andernfalls false.
      */
     @Override
     public boolean isValid(final InfrastruktureinrichtungDto value, final ConstraintValidatorContext context) {
