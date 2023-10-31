833c62d74e8515b82aa672de63a9693d42b61a3e
boal <56300801+boal@users.noreply.github.com>
2023-10-31 08:10:17 +0100

    ISI-626 Validierung wohnungsnaher Kinderkrippenplaetze Kindergartenplaetze Hortplaetze  (#109)
    * validator wohnungsnahe plaetze
    
    * validator wohnungsnahe plaetze
    
    * validator wohnungsnahe plaetze
    
    * add unit test
    
    * add gs nachmittagsbetreuung

---
+++ src/main/java/de/muenchen/isi/api/validation/WohnungsnahePlaetzeValidator.java
@@ -0,0 +1,75 @@
+package de.muenchen.isi.api.validation;
+
+import de.muenchen.isi.api.dto.infrastruktureinrichtung.GsNachmittagBetreuungDto;
+import de.muenchen.isi.api.dto.infrastruktureinrichtung.HausFuerKinderDto;
+import de.muenchen.isi.api.dto.infrastruktureinrichtung.InfrastruktureinrichtungDto;
+import de.muenchen.isi.api.dto.infrastruktureinrichtung.KindergartenDto;
+import de.muenchen.isi.api.dto.infrastruktureinrichtung.KinderkrippeDto;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+public class WohnungsnahePlaetzeValidator
+    implements ConstraintValidator<WohnungsnahePlaetzeValid, InfrastruktureinrichtungDto> {
+
+    /**
+     * Prüft, ob die Anzahl der wohungsnahen Plätze die Anzahl der insgesamt zur Verfügung stehenden Plätze der Kinderkrippe,
+     * des Kindergartens und des Horts übersteigt.
+     *
+     * @param value   {@link InfrastruktureinrichtungDto} zum Validieren.
+     * @param context in welchem die Validierung stattfindet.
+     * @return true, falls die Anzahl der wohungsnahen Plätze die Gesamtanzahl der Platze nicht übersteigt, andernfalls false.
+     */
+    @Override
+    public boolean isValid(final InfrastruktureinrichtungDto value, final ConstraintValidatorContext context) {
+        if (value != null) {
+            if (value instanceof HausFuerKinderDto) {
+                final var hausFuerKinder = ((HausFuerKinderDto) value);
+                return (
+                    this.areWohnungsnahePlatzeValid(
+                            hausFuerKinder.getAnzahlKinderkrippePlaetze(),
+                            hausFuerKinder.getWohnungsnaheKinderkrippePlaetze()
+                        ) &&
+                    this.areWohnungsnahePlatzeValid(
+                            hausFuerKinder.getAnzahlKindergartenPlaetze(),
+                            hausFuerKinder.getWohnungsnaheKindergartenPlaetze()
+                        ) &&
+                    this.areWohnungsnahePlatzeValid(
+                            hausFuerKinder.getAnzahlHortPlaetze(),
+                            hausFuerKinder.getWohnungsnaheHortPlaetze()
+                        )
+                );
+            } else if (value instanceof KindergartenDto) {
+                final var kindergarten = ((KindergartenDto) value);
+                return this.areWohnungsnahePlatzeValid(
+                        kindergarten.getAnzahlKindergartenPlaetze(),
+                        kindergarten.getWohnungsnaheKindergartenPlaetze()
+                    );
+            } else if (value instanceof KinderkrippeDto) {
+                final var kinderkrippe = ((KinderkrippeDto) value);
+                return this.areWohnungsnahePlatzeValid(
+                        kinderkrippe.getAnzahlKinderkrippePlaetze(),
+                        kinderkrippe.getWohnungsnaheKinderkrippePlaetze()
+                    );
+            } else if (value instanceof GsNachmittagBetreuungDto) {
+                final var kinderkrippe = ((GsNachmittagBetreuungDto) value);
+                return this.areWohnungsnahePlatzeValid(
+                        kinderkrippe.getAnzahlHortPlaetze(),
+                        kinderkrippe.getWohnungsnaheHortPlaetze()
+                    );
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    private boolean areWohnungsnahePlatzeValid(final Integer plaetzeGesamt, final Integer plaetzeWohnungsnah) {
+        return ObjectUtils.defaultIfNull(plaetzeWohnungsnah, 0) <= ObjectUtils.defaultIfNull(plaetzeGesamt, 0);
+    }
+}
