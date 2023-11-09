5f340eee2150acb8a745159c0f551e3e03fe5ef5
tiago.kocevar <tiago.kocevar@muenchen.de>
2023-11-09 16:51:44 +0100

    Testing added

--- src/main/java/de/muenchen/isi/infrastructure/repository/stammdaten/UmlegungFoerderartenRepository.java
+++ src/main/java/de/muenchen/isi/infrastructure/repository/stammdaten/UmlegungFoerderartenRepository.java
@@ -1,10 +1,14 @@
 package de.muenchen.isi.infrastructure.repository.stammdaten;
 
 import de.muenchen.isi.infrastructure.entity.stammdaten.UmlegungFoerderarten;
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UmlegungFoerderartenRepository extends JpaRepository<UmlegungFoerderarten, UUID> {
-    Optional<UmlegungFoerderarten> findBybezeichnung(final String bezeichnung);
+    Optional<UmlegungFoerderarten> findByBezeichnungAndGueltigAbBeforeOrderByGueltigAbDesc(
+        final String bezeichnung,
+        final LocalDate datum
+    );
 }
