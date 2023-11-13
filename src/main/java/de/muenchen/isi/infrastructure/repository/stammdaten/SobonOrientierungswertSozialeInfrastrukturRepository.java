5aea361f6a70579922fcdb002e3e0f2c39538277
tiago.kocevar <tiago.kocevar@muenchen.de>
2023-11-13 17:21:23 +0100

    Tests geupdated

--- src/main/java/de/muenchen/isi/infrastructure/repository/stammdaten/SobonOrientierungswertSozialeInfrastrukturRepository.java
+++ src/main/java/de/muenchen/isi/infrastructure/repository/stammdaten/SobonOrientierungswertSozialeInfrastrukturRepository.java
@@ -10,12 +10,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SobonOrientierungswertSozialeInfrastrukturRepository
     extends JpaRepository<SobonOrientierungswertSozialeInfrastruktur, UUID> {
+    /**
+     * Findet den Eintrag mit einer bestimmten Einrichtungstyp, Altersklasse und Förderart-Bezeichnung,
+     * bei dem das Gültigkeitsdatum vor oder am angegebenen Datum liegt, und sortiert die Ergebnisse nach dem
+     * Gültigkeitsdatum absteigend.
+     *
+     * @param gueltigAb             Das Gültigkeitsdatum, vor oder am dem gesucht werden soll (einschließlich).
+     * @param einrichtungstyp       Der Einrichtungstyp, nach dem gesucht werden soll.
+     * @param altersklasse          Die Altersklasse, nach der gesucht werden soll.
+     * @param foerderartBezeichnung Die Bezeichnung der Förderart, nach der gesucht werden soll.
+     * @return Ein {@link Optional} mit dem gefundenen Eintrag, oder ein leeres {@link Optional}, falls kein Eintrag gefunden wurde.
+     */
     Optional<
         SobonOrientierungswertSozialeInfrastruktur
-    > findByGueltigAbAndEinrichtungstypAndAltersklasseAndFoerderartBezeichnung(
-        final LocalDate gueltigAb,
+    > findFirstByEinrichtungstypAndAltersklasseAndFoerderartBezeichnungAndGueltigAbIsLessThanEqualOrderByGueltigAbDesc(
         final Einrichtungstyp einrichtungstyp,
         final Altersklasse altersklasse,
-        final String foerderartBezeichnung
+        final String foerderartBezeichnung,
+        final LocalDate gueltigAb
     );
 }
