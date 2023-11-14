9b444dea000a9580f36dbbb2e4d92511c8e9128e
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-11-14 12:41:44 +0100

    refactor import class

--- src/main/java/de/muenchen/isi/configuration/ImportStammdatenFromFile.java
+++ src/main/java/de/muenchen/isi/configuration/ImportStammdatenFromFile.java
@@ -7,16 +7,16 @@ import de.muenchen.isi.infrastructure.repository.stammdaten.SobonOrientierungswe
 import de.muenchen.isi.infrastructure.repository.stammdaten.StaedtebaulicheOrientierungswertRepository;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.List;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
@@ -29,19 +29,33 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 @Slf4j
 public class ImportStammdatenFromFile implements CommandLineRunner {
 
-    private static final String PATH = "csv/";
     private final StammdatenImportService stammdatenImportService;
+
     private final StaedtebaulicheOrientierungswertRepository staedtebaulicheOrientierungswertRepository;
+
     private final SobonOrientierungswertSozialeInfrastrukturRepository sobonOrientierungswertSozialeInfrastrukturRepository;
+
     private final Boolean deferDatasourceInit;
+
+    private final List<String> csvSobonOrientierungswertSozialeInfrastruktur;
+
+    private final List<String> csvStaedtebaulicheOrientierungswerte;
 
     public ImportStammdatenFromFile(
         @Value("${spring.jpa.defer-datasource-initialization:true}") final Boolean deferDatasourceInit,
+        @Value("${stammdaten.csv-locations.sobon-orientierungswerte-sozialinfrastruktur}") final List<
+            String
+        > csvSobonOrientierungswertSozialeInfrastruktur,
+        @Value("${stammdaten.csv-locations.staedtebauliche-orientierungswerte}") final List<
+            String
+        > csvStaedtebaulicheOrientierungswerte,
         final StammdatenImportService stammdatenImportService,
         final SobonOrientierungswertSozialeInfrastrukturRepository sobonOrientierungswertSozialeInfrastrukturRepository,
         final StaedtebaulicheOrientierungswertRepository staedtebaulicheOrientierungswertRepository
     ) {
         this.deferDatasourceInit = deferDatasourceInit;
+        this.csvSobonOrientierungswertSozialeInfrastruktur = csvSobonOrientierungswertSozialeInfrastruktur;
+        this.csvStaedtebaulicheOrientierungswerte = csvStaedtebaulicheOrientierungswerte;
         this.stammdatenImportService = stammdatenImportService;
         this.sobonOrientierungswertSozialeInfrastrukturRepository =
             sobonOrientierungswertSozialeInfrastrukturRepository;
@@ -54,61 +68,34 @@ public class ImportStammdatenFromFile implements CommandLineRunner {
      * Führt den Import von Stammdaten aus den angegebenen CSV-Dateien durch.
      */
     @Override
-    public void run(String... args) {
-        if ((deferDatasourceInit != null && deferDatasourceInit.booleanValue())) {
-            log.info("START LOADING STAMMDATEN");
-            this.clearDatabase();
-            this.addSobonJahre(PATH);
-            log.info("FINISHED LOADING STAMMDATEN");
+    public void run(String... args) throws CsvAttributeErrorException, FileImportFailedException, IOException {
+        if (BooleanUtils.isTrue(deferDatasourceInit)) {
+            log.info("START IMPORT SOBON STAMMDATEN");
+            this.sobonOrientierungswertSozialeInfrastrukturRepository.deleteAll();
+            this.staedtebaulicheOrientierungswertRepository.deleteAll();
+            for (final var csvFile : this.csvSobonOrientierungswertSozialeInfrastruktur) {
+                this.addSobonOrientierungswerteSozialeInfrastruktur(csvFile);
+                log.info("Stammdaten zu {} importiert", csvFile);
+            }
+            for (final var csvFile : this.csvStaedtebaulicheOrientierungswerte) {
+                this.addStaedtebaulicheOrientierungswerte(csvFile);
+                log.info("Stammdaten zu {} importiert", csvFile);
+            }
+            log.info("FINISHED IMPORT SOBON STAMMDATEN");
         }
     }
 
     /**
-     * Löscht alle vorhandenen Datensätze in der Datenbank.
-     */
-    private void clearDatabase() {
-        this.sobonOrientierungswertSozialeInfrastrukturRepository.deleteAll();
-        this.staedtebaulicheOrientierungswertRepository.deleteAll();
-    }
-
-    /**
-     * Fügt die Sobon-Jahre in die Datenbank ein.
+     * Importiert die Sobon-Orientierungswerte für soziale Infrastruktur aus der angegebenen CSV-Datei.
      *
-     * @param dateipfad Der Dateipfad zum Verzeichnis der CSV-Dateien.
-     */
-    public void addSobonJahre(String dateipfad) {
-        try {
-            this.addSobonOrientierungswerte(dateipfad + "SoBoNOrientierungswerteSozialeInfrastruktur2014.csv");
-            this.addStaedtebaulicheOrientierungswerte(dateipfad + "StaedteBaulicheOrientierungswerte2014.csv");
-            log.info("--------------------2014 Eingespielt----------------------------");
-            this.addSobonOrientierungswerte(dateipfad + "SoBoNOrientierungswerteSozialeInfrastruktur2017.csv");
-            this.addStaedtebaulicheOrientierungswerte(dateipfad + "StaedteBaulicheOrientierungswerte2017.csv");
-            log.info("--------------------2017 Eingespielt----------------------------");
-            this.addSobonOrientierungswerte(dateipfad + "SoBoNOrientierungswerteSozialeInfrastruktur2022.csv");
-            this.addStaedtebaulicheOrientierungswerte(dateipfad + "StaedteBaulicheOrientierungswerte2022.csv");
-            log.info("--------------------2022 Eingespielt----------------------------");
-        } catch (CsvAttributeErrorException e) {
-            log.error(e.getMessage());
-        } catch (FileImportFailedException e) {
-            log.error(e.getMessage());
-        } catch (FileNotFoundException e) {
-            log.error(e.getMessage());
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    /**
-     * Importiert die Sobon-Orientierungswerte aus der angegebenen CSV-Datei.
-     *
-     * @param filePath Der Dateipfad zur Sobon-Orientierungswerte CSV-Datei.
+     * @param filePath Der Dateipfad zur Sobon-Orientierungswerte für soziale Infrastruktur CSV-Datei.
      * @throws IOException                Wenn ein Fehler beim Lesen der Datei auftritt.
      * @throws CsvAttributeErrorException Wenn ein Fehler in den CSV-Attributen auftritt.
      * @throws FileImportFailedException  Wenn der Import der Datei fehlschlägt.
      */
-    public void addSobonOrientierungswerte(String filePath)
+    public void addSobonOrientierungswerteSozialeInfrastruktur(String filePath)
         throws IOException, CsvAttributeErrorException, FileImportFailedException {
-        MultipartFile multipartFile = createMultipartFile(filePath);
+        final var multipartFile = this.createMultipartFile(filePath);
         this.stammdatenImportService.importSobonOrientierungswerteSozialeInfrastruktur(multipartFile);
     }
 
@@ -122,7 +109,7 @@ public class ImportStammdatenFromFile implements CommandLineRunner {
      */
     public void addStaedtebaulicheOrientierungswerte(String filePath)
         throws IOException, CsvAttributeErrorException, FileImportFailedException {
-        MultipartFile multipartFile = createMultipartFile(filePath);
+        final var multipartFile = this.createMultipartFile(filePath);
         this.stammdatenImportService.importStaedtebaulicheOrientierungswerte(multipartFile);
     }
 
@@ -131,28 +118,21 @@ public class ImportStammdatenFromFile implements CommandLineRunner {
      *
      * @param filePath Der Dateipfad zur CSV-Datei.
      * @return Das MultipartFile-Objekt.
-     * @throws IOException Wenn ein Fehler beim Lesen der Datei auftritt.
+     * @throws IOException sobald ein Fehler beim Lesen der Datei auftritt.
      */
     public MultipartFile createMultipartFile(String filePath) throws IOException {
-        FileItem fileItem = null;
-        InputStream inputStream = new ClassPathResource(filePath).getInputStream();
-
-        if (inputStream != null) {
-            File tempFile = File.createTempFile("tempfile", ".tmp");
-            FileUtils.copyInputStreamToFile(inputStream, tempFile);
-            fileItem =
-                new DiskFileItem(
-                    "tempfile",
-                    Files.probeContentType(tempFile.toPath()),
-                    false,
-                    tempFile.getName(),
-                    (int) tempFile.length(),
-                    tempFile.getParentFile()
-                );
-            IOUtils.copy(new FileInputStream(tempFile), fileItem.getOutputStream());
-        } else {
-            throw new FileNotFoundException("Datei wurde nicht gefunden");
-        }
+        final var inputStream = new ClassPathResource(filePath).getInputStream();
+        final var tempFile = File.createTempFile("tempfile", ".tmp");
+        FileUtils.copyInputStreamToFile(inputStream, tempFile);
+        final FileItem fileItem = new DiskFileItem(
+            "tempfile",
+            Files.probeContentType(tempFile.toPath()),
+            false,
+            tempFile.getName(),
+            (int) tempFile.length(),
+            tempFile.getParentFile()
+        );
+        IOUtils.copy(new FileInputStream(tempFile), fileItem.getOutputStream());
         return new CommonsMultipartFile(fileItem);
     }
 }
