f1b3183bff648645ba9d2f87b402e5528890448e
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-11-14 13:04:01 +0100

    adapt year string to date converter

--- src/main/java/de/muenchen/isi/infrastructure/adapter/JahrConverter.java
+++ src/main/java/de/muenchen/isi/infrastructure/adapter/JahrConverter.java
@@ -1,10 +1,7 @@
 package de.muenchen.isi.infrastructure.adapter;
 
 import com.opencsv.bean.AbstractBeanField;
-import com.opencsv.exceptions.CsvConstraintViolationException;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import lombok.extern.slf4j.Slf4j;
@@ -13,13 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 public class JahrConverter extends AbstractBeanField {
 
     @Override
-    protected Object convert(String s) throws CsvDataTypeMismatchException, CsvConstraintViolationException {
-        DateTimeFormatter dtf = new DateTimeFormatterBuilder()
+    protected Object convert(String value) {
+        final var dateTimeFormatter = new DateTimeFormatterBuilder()
             .appendPattern("yyyy")
             .parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
             .parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
             .toFormatter();
-        LocalDate ldt = LocalDate.parse(s, dtf);
-        return ldt;
+        return LocalDate.parse(value, dateTimeFormatter);
     }
 }
