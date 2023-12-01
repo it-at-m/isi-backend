fa548ee320c407010518e0e2e9f4a9184d6ef4e8
Bagiera <ex.kempf02@muenchen.de>
2023-12-01 13:36:08 +0100

    Berechnung SoBoN-ursächlicher Wohneinheiten

--- src/main/java/de/muenchen/isi/domain/service/calculation/SobonursaechlicheWohneinheitenService.java
+++ src/main/java/de/muenchen/isi/domain/service/calculation/SobonursaechlicheWohneinheitenService.java
@@ -3,7 +3,7 @@ package de.muenchen.isi.domain.service.calculation;
 import de.muenchen.isi.domain.model.BauabschnittModel;
 import de.muenchen.isi.domain.model.BaurateModel;
 import de.muenchen.isi.domain.model.FoerderartModel;
-import de.muenchen.isi.domain.model.calculation.PlanungsursachlicheWohneinheitenModel;
+import de.muenchen.isi.domain.model.calculation.WohneinheitenProFoerderartProJahrModel;
 import de.muenchen.isi.infrastructure.entity.enums.lookup.SobonOrientierungswertJahr;
 import de.muenchen.isi.infrastructure.repository.stammdaten.StaedtebaulicheOrientierungswertRepository;
 import java.math.BigDecimal;
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class SobonursaechlicheWohneinheitenService {
+public class SobonursaechlicheWohneinheitenService /*extends WohneinheitenCalculationService*/{
 
     private final FoerdermixUmlageService foerdermixUmlageService;
 
@@ -38,10 +38,7 @@ public class SobonursaechlicheWohneinheitenService {
      * Die Fördermixe aller Bauraten werden vor der Berechnung umgelegt, siehe {@link FoerdermixUmlageService}.
      *
      * Ist das Ergebnis aus dieser Rechnung über 1000 Wohneinheiten, werden die Wohneinheiten auf mehrere Bauraten (mehrere Jahre) aufgeteilt.
-     * Es wird immer bei 1000 WE ein Schnitt gemacht. Im angehängten Dokument ist der Rechenweg abgebildet.
-     *
-     * Tabelle 2: Errechnung der Wohneinheiten je Förderart für die Jahre mit 1000 Wohneinheiten (siehe Formel im gelben Feld).
-     * Tabelle 3: Errechnung der letzten Bauraten, die keine 1000 Wohneinheiten mehr hat (siehe Formel im gelben Feld).
+     * Es wird immer bei 1000 WE ein Schnitt gemacht.
      *
      * Für die Summenbildung gibt es eine zusätzliche "Förderart", die die Summen aller Förderarten pro Jahr enthält.
      * Außerdem gibt es zusätzliche Jahre pro Förderart, die über 10, 15 & 20 Jahre die Wohneinheiten aufsummieren.
@@ -52,15 +49,15 @@ public class SobonursaechlicheWohneinheitenService {
      *         der angelegt wurde, das erste Baugebiet, das angelegt wurde und dann davon die erste Baurate verwendet.
      * @param sobonJahr Das SoBoN-Jahr, welches die städtebaulichen Orientierungswerte diktiert.
      * @param gueltigAb Das Gültigkeitsdatum der Stammdaten, welche die Umlegung diktieren.
-     * @return Eine Liste von {@link PlanungsursachlicheWohneinheitenModel}, welche alle Wohneinheiten pro Förderart und Jahr darstellt.
+     * @return Eine Liste von {@link WohneinheitenProFoerderartProJahrModel}, welche alle Wohneinheiten pro Förderart und Jahr darstellt.
      */
-    public List<PlanungsursachlicheWohneinheitenModel> calculateSobonursaechlicheWohneinheiten(
+    public List<WohneinheitenProFoerderartProJahrModel> calculateSobonursaechlicheWohneinheiten(
         final BigDecimal sobonGF,
         final BaurateModel baurate,
         final SobonOrientierungswertJahr sobonJahr,
         final LocalDate gueltigAb
     ) {
-        final var sobonsursachlicheWohneinheitenList = new ArrayList<PlanungsursachlicheWohneinheitenModel>();
+        final var sobonsursachlicheWohneinheitenList = new ArrayList<WohneinheitenProFoerderartProJahrModel>();
 
         // Umlegen von Förderarten
         baurate.setFoerdermix(foerdermixUmlageService.legeFoerdermixUm(baurate.getFoerdermix(), gueltigAb));
@@ -70,7 +67,7 @@ public class SobonursaechlicheWohneinheitenService {
 
         // Summe der Wohneinheiten aller Förderarten
         BigDecimal summeWE = BigDecimal.ZERO;
-        for (PlanungsursachlicheWohneinheitenModel sobonursaechlicheWE : sobonsursachlicheWohneinheitenList) {
+        for (WohneinheitenProFoerderartProJahrModel sobonursaechlicheWE : sobonsursachlicheWohneinheitenList) {
             summeWE = summeWE.add(sobonursaechlicheWE.getWohneinheiten());
         }
 
@@ -79,13 +76,13 @@ public class SobonursaechlicheWohneinheitenService {
             sobonsursachlicheWohneinheitenList.clear();
 
             BigDecimal gfWohnen1000 = sobonGF.multiply(
-                BigDecimal.valueOf(1000).divide(summeWE, CalculationService.DIVISION_SCALE, RoundingMode.HALF_EVEN)
+                new BigDecimal(1000).divide(summeWE, CalculationService.DIVISION_SCALE, RoundingMode.HALF_EVEN)
             );
-            // Anzahl 1000er-Blöcke/Jahre summeWE / 1000 = Ergebnis abrunden
+            // Anzahl 1000er-Blöcke/Jahre = summeWE / 1000 = Ergebnis abrunden
             Integer anzahl = summeWE
                 .divide(new BigDecimal(1000), CalculationService.DIVISION_SCALE, RoundingMode.HALF_DOWN)
                 .intValue();
-            System.out.println("anzahl: " + anzahl);
+
             for (int i = 0; i < anzahl; i++) {
                 calculateWohneinheiten(
                     baurate,
@@ -111,36 +108,7 @@ public class SobonursaechlicheWohneinheitenService {
             );
         }
 
-        // TODO doch nur eine Liste verwenden, damit Summenbildung an einem Ort stattfinden kann
-        // Hinzufügen von zusätzlichen Jahren pro Förderart, welche eine Anzahl von Jahren über einen Zeitraum summieren.
-        sobonsursachlicheWohneinheitenList.sort(Comparator.comparingInt(a -> Integer.parseInt(a.getJahr())));
-        final var firstYear = Integer.parseInt(sobonsursachlicheWohneinheitenList.get(0).getJahr());
-        final var sumsPerFoerderart = new ArrayList<PlanungsursachlicheWohneinheitenModel>();
-        for (final var sobonursachlicheWohneinheiten : sobonsursachlicheWohneinheitenList) {
-            for (final var period : SUMMARY_PERIODS) {
-                if (Integer.parseInt(sobonursachlicheWohneinheiten.getJahr()) <= firstYear + period - 1) {
-                    mergeSobonursaechlicheWohneinheiten(
-                        sumsPerFoerderart,
-                        sobonursachlicheWohneinheiten.getFoerderart(),
-                        String.format(SUMMARY_OVER_PERIOD_NAME, period),
-                        sobonursachlicheWohneinheiten.getWohneinheiten()
-                    );
-                }
-            }
-        }
-        sobonsursachlicheWohneinheitenList.addAll(sumsPerFoerderart);
-
-        // Hinzufügen einer Förderart mit den Summen aller Förderarten pro Jahr.
-        final var sumsPerYear = new ArrayList<PlanungsursachlicheWohneinheitenModel>();
-        for (final var sobonursachlicheWohneinheiten : sobonsursachlicheWohneinheitenList) {
-            mergeSobonursaechlicheWohneinheiten(
-                sumsPerYear,
-                SUMMARY_NAME,
-                sobonursachlicheWohneinheiten.getJahr(),
-                sobonursachlicheWohneinheiten.getWohneinheiten()
-            );
-        }
-        sobonsursachlicheWohneinheitenList.addAll(sumsPerYear);
+        // TODO Summenbildung
 
         return sobonsursachlicheWohneinheitenList;
     }
@@ -155,7 +123,7 @@ public class SobonursaechlicheWohneinheitenService {
         final Integer jahr,
         final BigDecimal sobonGF,
         final LocalDate sobonJahr,
-        final List<PlanungsursachlicheWohneinheitenModel> sobonsursachlicheWohneinheitenList
+        final List<WohneinheitenProFoerderartProJahrModel> sobonsursachlicheWohneinheitenList
     ) {
         // Berechnen der Wohneinheiten pro Förderart und Jahr
         for (final var foerderart : baurate.getFoerdermix().getFoerderarten()) {
@@ -174,37 +142,12 @@ public class SobonursaechlicheWohneinheitenService {
             }
 
             sobonsursachlicheWohneinheitenList.add(
-                new PlanungsursachlicheWohneinheitenModel(
+                new WohneinheitenProFoerderartProJahrModel(
                     foerderart.getBezeichnung(),
                     String.valueOf(baurate.getJahr() + jahr),
                     wohneinheiten
                 )
             );
-        }
-    }
-
-    protected void mergeSobonursaechlicheWohneinheiten(
-        final List<PlanungsursachlicheWohneinheitenModel> sobonursachlicheWohneinheitenList,
-        final String foerderart,
-        final String jahr,
-        final BigDecimal wohneinheiten
-    ) {
-        final var sobonursaechlicheWohneinheitenOptional = sobonursachlicheWohneinheitenList
-            .stream()
-            .filter(sobonursachlicheWohneinheiten ->
-                sobonursachlicheWohneinheiten.getFoerderart().equals(foerderart) &&
-                sobonursachlicheWohneinheiten.getJahr().equals(jahr)
-            )
-            .findAny();
-
-        if (sobonursaechlicheWohneinheitenOptional.isEmpty()) {
-            sobonursachlicheWohneinheitenList.add(
-                new PlanungsursachlicheWohneinheitenModel(foerderart, jahr, wohneinheiten)
-            );
-        } else {
-            sobonursaechlicheWohneinheitenOptional
-                .get()
-                .setWohneinheiten(sobonursaechlicheWohneinheitenOptional.get().getWohneinheiten().add(wohneinheiten));
         }
     }
 }
--- src/test/java/de/muenchen/isi/domain/service/calculation/SobonursaechlicheWohneinheitenServiceTest.java
+++ src/test/java/de/muenchen/isi/domain/service/calculation/SobonursaechlicheWohneinheitenServiceTest.java
@@ -13,7 +13,7 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 import de.muenchen.isi.domain.model.BaurateModel;
 import de.muenchen.isi.domain.model.FoerderartModel;
 import de.muenchen.isi.domain.model.FoerdermixModel;
-import de.muenchen.isi.domain.model.calculation.PlanungsursachlicheWohneinheitenModel;
+import de.muenchen.isi.domain.model.calculation.WohneinheitenProFoerderartProJahrModel;
 import de.muenchen.isi.infrastructure.entity.enums.lookup.SobonOrientierungswertJahr;
 import de.muenchen.isi.infrastructure.repository.stammdaten.StaedtebaulicheOrientierungswertRepository;
 import de.muenchen.isi.infrastructure.repository.stammdaten.UmlegungFoerderartenRepository;
@@ -60,10 +60,6 @@ public class SobonursaechlicheWohneinheitenServiceTest {
         );
 
         // Fördermixe
-        final var FF100 = new FoerderartModel();
-        FF100.setBezeichnung(FF);
-        FF100.setAnteilProzent(BigDecimal.ONE);
-
         final var FF50 = new FoerderartModel();
         FF50.setBezeichnung(FF);
         FF50.setAnteilProzent(new BigDecimal("0.5"));
@@ -72,49 +68,9 @@ public class SobonursaechlicheWohneinheitenServiceTest {
         MM30.setBezeichnung(MM);
         MM30.setAnteilProzent(new BigDecimal("0.3"));
 
-        final var MM75 = new FoerderartModel();
-        MM75.setBezeichnung(MM);
-        MM75.setAnteilProzent(new BigDecimal("0.75"));
-
         final var EOF20 = new FoerderartModel();
         EOF20.setBezeichnung(EOF);
         EOF20.setAnteilProzent(new BigDecimal("0.2"));
-
-        final var EOF25 = new FoerderartModel();
-        EOF25.setBezeichnung(EOF);
-        EOF25.setAnteilProzent(new BigDecimal("0.25"));
-
-        final var FH0 = new FoerderartModel();
-        FH0.setBezeichnung(FH);
-        FH0.setAnteilProzent(BigDecimal.ZERO);
-
-        final var FH50 = new FoerderartModel();
-        FH50.setBezeichnung(FH);
-        FH50.setAnteilProzent(new BigDecimal("0.5"));
-
-        final var PMB75 = new FoerderartModel();
-        PMB75.setBezeichnung(PMB);
-        PMB75.setAnteilProzent(new BigDecimal("0.75"));
-
-        final var KMB25 = new FoerderartModel();
-        KMB25.setBezeichnung(KMB);
-        KMB25.setAnteilProzent(new BigDecimal("0.25"));
-
-        final var BAU50 = new FoerderartModel();
-        BAU50.setBezeichnung(BAU);
-        BAU50.setAnteilProzent(new BigDecimal("0.5"));
-
-        final var foerdermixFF100 = new FoerdermixModel();
-        foerdermixFF100.setFoerderarten(List.of(FF100));
-
-        final var foerdermixFH50BAU50 = new FoerdermixModel();
-        foerdermixFH50BAU50.setFoerderarten(List.of(FH50, BAU50));
-
-        final var foerdermixMM75EOF25 = new FoerdermixModel();
-        foerdermixMM75EOF25.setFoerderarten(List.of(MM75, EOF25));
-
-        final var foerdermixPMB75KMB25 = new FoerdermixModel();
-        foerdermixPMB75KMB25.setFoerderarten(List.of(PMB75, KMB25));
 
         final var meinFoerdermix = new FoerdermixModel();
         meinFoerdermix.setFoerderarten(List.of(FF50, EOF20, MM30));
@@ -125,9 +81,9 @@ public class SobonursaechlicheWohneinheitenServiceTest {
 
         // 1. Test unter 1000 Wohneinheiten
         final var expected = List.of(
-            new PlanungsursachlicheWohneinheitenModel(FF, "2024", new BigDecimal("157.8947368421")),
-            new PlanungsursachlicheWohneinheitenModel(EOF, "2024", new BigDecimal("66.6666666667")),
-            new PlanungsursachlicheWohneinheitenModel(MM, "2024", new BigDecimal("90.0000000000"))
+            new WohneinheitenProFoerderartProJahrModel(FF, "2024", new BigDecimal("157.8947368421")),
+            new WohneinheitenProFoerderartProJahrModel(EOF, "2024", new BigDecimal("66.6666666667")),
+            new WohneinheitenProFoerderartProJahrModel(MM, "2024", new BigDecimal("90.0000000000"))
         );
 
         final var actual = sobonursaechlicheWohneinheitenService.calculateSobonursaechlicheWohneinheiten(
@@ -138,23 +94,23 @@ public class SobonursaechlicheWohneinheitenServiceTest {
         );
 
         System.out.println("----------------- TEST A ---------------------------------");
-        for (PlanungsursachlicheWohneinheitenModel weModel : actual) {
+        for (WohneinheitenProFoerderartProJahrModel weModel : actual) {
             System.out.println("Jahr: " + weModel.getJahr()); // Jahr/Info aus Baurate
             System.out.println("Förderart: " + weModel.getFoerderart());
             System.out.println("Wohneinheiten: " + weModel.getWohneinheiten());
             System.out.println("--------------------------------------------------");
         }
 
-        //assertThat(actual, containsInAnyOrder(expected.toArray()));
+        assertThat(actual, containsInAnyOrder(expected.toArray()));
 
         // 2. Test über 1000 Wohneinheiten mit einem 1000er Block/Jahr
         final var expectedB = List.of(
-            new PlanungsursachlicheWohneinheitenModel(FF, "2024", new BigDecimal("501.9520356842")),
-            new PlanungsursachlicheWohneinheitenModel(EOF, "2024", new BigDecimal("211.9353039556")),
-            new PlanungsursachlicheWohneinheitenModel(MM, "2024", new BigDecimal("286.1126603400")),
-            new PlanungsursachlicheWohneinheitenModel(FF, "2025", new BigDecimal("182.2584906316")),
-            new PlanungsursachlicheWohneinheitenModel(EOF, "2025", new BigDecimal("76.9535849333")),
-            new PlanungsursachlicheWohneinheitenModel(MM, "2025", new BigDecimal("103.8873396600"))
+            new WohneinheitenProFoerderartProJahrModel(FF, "2024", new BigDecimal("501.9520356842")),
+            new WohneinheitenProFoerderartProJahrModel(EOF, "2024", new BigDecimal("211.9353039556")),
+            new WohneinheitenProFoerderartProJahrModel(MM, "2024", new BigDecimal("286.1126603400")),
+            new WohneinheitenProFoerderartProJahrModel(FF, "2025", new BigDecimal("182.2584906316")),
+            new WohneinheitenProFoerderartProJahrModel(EOF, "2025", new BigDecimal("76.9535849333")),
+            new WohneinheitenProFoerderartProJahrModel(MM, "2025", new BigDecimal("103.8873396600"))
         );
 
         final var actualB = sobonursaechlicheWohneinheitenService.calculateSobonursaechlicheWohneinheiten(
@@ -165,26 +121,26 @@ public class SobonursaechlicheWohneinheitenServiceTest {
         );
 
         System.out.println("----------------- TEST B ---------------------------------");
-        for (PlanungsursachlicheWohneinheitenModel weModel : actualB) {
+        for (WohneinheitenProFoerderartProJahrModel weModel : actualB) {
             System.out.println("Jahr: " + weModel.getJahr()); // Jahr/Info aus Baurate
             System.out.println("Förderart: " + weModel.getFoerderart());
             System.out.println("Wohneinheiten: " + weModel.getWohneinheiten());
             System.out.println("--------------------------------------------------");
         }
 
-        // assertThat(actualB, containsInAnyOrder(expectedB.toArray()));
+        assertThat(actualB, containsInAnyOrder(expectedB.toArray()));
 
         // 3. Test über 1000 Wohneinheiten mit zwei 1000er Block/Jahr
         final var expectedC = List.of(
-            new PlanungsursachlicheWohneinheitenModel(FF, "2024", new BigDecimal("501.9520357105")),
-            new PlanungsursachlicheWohneinheitenModel(EOF, "2024", new BigDecimal("211.9353039667")),
-            new PlanungsursachlicheWohneinheitenModel(MM, "2024", new BigDecimal("286.1126603550")),
-            new PlanungsursachlicheWohneinheitenModel(FF, "2025", new BigDecimal("501.9520357105")),
-            new PlanungsursachlicheWohneinheitenModel(EOF, "2025", new BigDecimal("211.9353039667")),
-            new PlanungsursachlicheWohneinheitenModel(MM, "2025", new BigDecimal("286.1126603550")),
-            new PlanungsursachlicheWohneinheitenModel(FF, "2026", new BigDecimal("206.6222443684")),
-            new PlanungsursachlicheWohneinheitenModel(EOF, "2026", new BigDecimal("87.2405031778")),
-            new PlanungsursachlicheWohneinheitenModel(MM, "2026", new BigDecimal("117.7746792900"))
+            new WohneinheitenProFoerderartProJahrModel(FF, "2024", new BigDecimal("501.9520357105")),
+            new WohneinheitenProFoerderartProJahrModel(EOF, "2024", new BigDecimal("211.9353039667")),
+            new WohneinheitenProFoerderartProJahrModel(MM, "2024", new BigDecimal("286.1126603550")),
+            new WohneinheitenProFoerderartProJahrModel(FF, "2025", new BigDecimal("501.9520357105")),
+            new WohneinheitenProFoerderartProJahrModel(EOF, "2025", new BigDecimal("211.9353039667")),
+            new WohneinheitenProFoerderartProJahrModel(MM, "2025", new BigDecimal("286.1126603550")),
+            new WohneinheitenProFoerderartProJahrModel(FF, "2026", new BigDecimal("206.6222443684")),
+            new WohneinheitenProFoerderartProJahrModel(EOF, "2026", new BigDecimal("87.2405031778")),
+            new WohneinheitenProFoerderartProJahrModel(MM, "2026", new BigDecimal("117.7746792900"))
         );
 
         final var actualC = sobonursaechlicheWohneinheitenService.calculateSobonursaechlicheWohneinheiten(
@@ -195,13 +151,12 @@ public class SobonursaechlicheWohneinheitenServiceTest {
         );
 
         System.out.println("----------------- TEST C ---------------------------------");
-        for (PlanungsursachlicheWohneinheitenModel weModel : actualC) {
+        for (WohneinheitenProFoerderartProJahrModel weModel : actualC) {
             System.out.println("Jahr: " + weModel.getJahr()); // Jahr/Info aus Baurate
             System.out.println("Förderart: " + weModel.getFoerderart());
             System.out.println("Wohneinheiten: " + weModel.getWohneinheiten());
             System.out.println("--------------------------------------------------");
         }
-        //assertThat(actualC, containsInAnyOrder(expectedC.toArray()));
-
+        assertThat(actualC, containsInAnyOrder(expectedC.toArray()));
     }
 }
