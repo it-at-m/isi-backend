b87121d1984dd6ac12faa6bec6052e92a75df2f8
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-01-16 10:28:31 +0100

    resolve merge conflicts

--- src/main/java/de/muenchen/isi/api/dto/abfrageBedarfsmeldungErfolgt/BaugenehmigungsverfahrenBedarfsmeldungErfolgtDto.java
+++ src/main/java/de/muenchen/isi/api/dto/abfrageBedarfsmeldungErfolgt/BaugenehmigungsverfahrenBedarfsmeldungErfolgtDto.java
@@ -1,10 +1,10 @@
 package de.muenchen.isi.api.dto.abfrageBedarfsmeldungErfolgt;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
-import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
