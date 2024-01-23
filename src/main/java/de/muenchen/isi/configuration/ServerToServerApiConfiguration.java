82e1c7c6be7c3c20c855b393539e3639e11b2cd7
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-01-23 12:01:08 +0100

    add api configuration

---
+++ src/main/java/de/muenchen/isi/configuration/ServerToServerApiConfiguration.java
@@ -0,0 +1,55 @@
+package de.muenchen.isi.configuration;
+
+import de.muenchen.isi.reporting.client.ApiClient;
+import de.muenchen.isi.reporting.eai.client.api.AbfrageReportingEaiApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ServerToServerApiConfiguration {
+
+    @Value("${isi.reporting.url}")
+    private String isiReportingUrl;
+
+    @Bean
+    @Profile("!no-security")
+    public AbfrageReportingEaiApi securedAbfrageReportingEaiApi(
+        final ClientRegistrationRepository clientRegistrationRepository,
+        final OAuth2AuthorizedClientService authorizedClientService
+    ) {
+        final var webClient = webClient(clientRegistrationRepository, authorizedClientService);
+        final var apiClient = new ApiClient(webClient);
+        apiClient.setBasePath(isiReportingUrl);
+        return new AbfrageReportingEaiApi(apiClient);
+    }
+
+    @Bean
+    @Profile("no-security")
+    public AbfrageReportingEaiApi abfrageReportingEaiApi() {
+        final var webClient = WebClient.builder().build();
+        final var apiClient = new ApiClient(webClient);
+        apiClient.setBasePath(isiReportingUrl);
+        return new AbfrageReportingEaiApi(apiClient);
+    }
+
+    private WebClient webClient(
+        final ClientRegistrationRepository clientRegistrationRepository,
+        final OAuth2AuthorizedClientService authorizedClientService
+    ) {
+        final var oauth = new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+            new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+                clientRegistrationRepository,
+                authorizedClientService
+            )
+        );
+        oauth.setDefaultClientRegistrationId("server-to-server");
+        return WebClient.builder().apply(oauth.oauth2Configuration()).build();
+    }
+}
