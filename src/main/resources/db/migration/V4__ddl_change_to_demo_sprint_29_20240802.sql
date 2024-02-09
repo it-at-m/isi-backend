6d49a30e5bd816a8e4ef4a048b0ef21818924a14
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-02-09 08:33:57 +0100

    add alter column statements

--- src/main/resources/db/migration/V4__ddl_change_to_demo_sprint_29_20240802.sql
+++ src/main/resources/db/migration/V4__ddl_change_to_demo_sprint_29_20240802.sql
@@ -30,6 +30,7 @@ ALTER TABLE isidbuser.abfragevariante_bauleitplanverfahren
     ADD COLUMN ausglstr_bdrf_mtvrsrg_in_bsthnd_einr_nch_asbau_kita boolean,
     ADD COLUMN ausglstr_bdrf_mtvrsrg_in_bsthnd_einr_nch_asbau_schule boolean,
     ADD COLUMN ausglstr_bdrf_mtvrsrg_in_bsthnd_einr_schule boolean,
+    ALTER COLUMN satzungsbeschluss timestamp(6) without time zone,
     ADD COLUMN stammdaten_gueltig_ab timestamp(6) without time zone,
     ADD COLUMN hinweis_versorgung character varying(1000),
     ADD CONSTRAINT abfragevariante_bauleitplanv_sobon_orientierungswert_jahr_check CHECK (((sobon_orientierungswert_jahr)::text = ANY ((ARRAY['UNSPECIFIED'::character varying, 'JAHR_2014'::character varying, 'JAHR_2017'::character varying, 'JAHR_2022'::character varying, 'STANDORTABFRAGE'::character varying])::text[])));
@@ -46,6 +47,7 @@ ALTER TABLE isidbuser.abfragevariante_weiteres_verfahren
     ADD COLUMN ausglstr_bdrf_mtvrsrg_in_bsthnd_einr_nch_asbau_kita boolean,
     ADD COLUMN ausglstr_bdrf_mtvrsrg_in_bsthnd_einr_nch_asbau_schule boolean,
     ADD COLUMN ausglstr_bdrf_mtvrsrg_in_bsthnd_einr_schule boolean,
+    ALTER COLUMN satzungsbeschluss timestamp(6) without time zone,
     ADD COLUMN stammdaten_gueltig_ab timestamp(6) without time zone,
     ADD COLUMN hinweis_versorgung character varying(1000),
     ADD CONSTRAINT abfragevariante_weiteres_ver_sobon_orientierungswert_jahr_check CHECK (((sobon_orientierungswert_jahr)::text = ANY ((ARRAY['UNSPECIFIED'::character varying, 'JAHR_2014'::character varying, 'JAHR_2017'::character varying, 'JAHR_2022'::character varying, 'STANDORTABFRAGE'::character varying])::text[])));
@@ -57,10 +59,12 @@ ALTER TABLE isidbuser.baugebiet
     ADD CONSTRAINT baugebiet_art_bauliche_nutzung_check CHECK (((art_bauliche_nutzung >= 0) AND (art_bauliche_nutzung <= 7)));
 
 ALTER TABLE isidbuser.baugenehmigungsverfahren
+    ALTER COLUMN  frist_bearbeitung timestamp(6) without time zone NOT NULL,
     ADD CONSTRAINT baugenehmigungsverfahren_stand_verfahren_check CHECK (((stand_verfahren)::text = ANY ((ARRAY['UNSPECIFIED'::character varying, 'VORBEREITUNG_ECKDATENBESCHLUSS'::character varying, 'VORBEREITUNG_WETTBEWERBAUSLOBUNG'::character varying, 'VORBEREITUNG_AUFSTELLUNGSBESCHLUSS'::character varying, 'VORBEREITUNG_BILLIGUNGSBESCHLUSS_STAEDTEBAULICHER_VERTRAG'::character varying, 'VORLIEGENDER_SATZUNGSBESCHLUSS'::character varying, 'RECHTSVERBINDLICHKEIT_AMTSBLATT'::character varying, 'AUFTEILUNGSPLAN'::character varying, 'VORBEREITUNG_VORBESCHEID'::character varying, 'VORBEREITUNG_BAUGENEHMIGUNG'::character varying, 'VORABFRAGE_OHNE_KONKRETEN_STAND'::character varying, 'STRUKTURKONZEPT'::character varying, 'RAHMENPLANUNG'::character varying, 'POTENTIALUNTERSUCHUNG'::character varying, 'STAEDTEBAULICHE_SANIERUNGSMASSNAHME'::character varying, 'STAEDTEBAULICHE_ENTWICKLUNGSMASSNAHME'::character varying, 'INFO_FEHLT'::character varying, 'FREIE_EINGABE'::character varying, 'STANDORTABFRAGE'::character varying])::text[]))),
     ADD CONSTRAINT baugenehmigungsverfahren_status_abfrage_check CHECK (((status_abfrage)::text = ANY ((ARRAY['ANGELEGT'::character varying, 'OFFEN'::character varying, 'IN_BEARBEITUNG_SACHBEARBEITUNG'::character varying, 'IN_BEARBEITUNG_FACHREFERATE'::character varying, 'BEDARFSMELDUNG_ERFOLGT'::character varying, 'ERLEDIGT_MIT_FACHREFERAT'::character varying, 'ERLEDIGT_OHNE_FACHREFERAT'::character varying, 'ABBRUCH'::character varying])::text[])));
 
 ALTER TABLE isidbuser.bauleitplanverfahren
+    ALTER COLUMN  frist_bearbeitung timestamp(6) without time zone NOT NULL,
     ADD CONSTRAINT bauleitplanverfahren_sobon_relevant_check2 CHECK (((sobon_relevant)::text = ANY ((ARRAY['UNSPECIFIED'::character varying, 'TRUE'::character varying, 'FALSE'::character varying])::text[]))),
     ADD CONSTRAINT bauleitplanverfahren_stand_verfahren_check CHECK (((stand_verfahren)::text = ANY ((ARRAY['UNSPECIFIED'::character varying, 'VORBEREITUNG_ECKDATENBESCHLUSS'::character varying, 'VORBEREITUNG_WETTBEWERBAUSLOBUNG'::character varying, 'VORBEREITUNG_AUFSTELLUNGSBESCHLUSS'::character varying, 'VORBEREITUNG_BILLIGUNGSBESCHLUSS_STAEDTEBAULICHER_VERTRAG'::character varying, 'VORLIEGENDER_SATZUNGSBESCHLUSS'::character varying, 'RECHTSVERBINDLICHKEIT_AMTSBLATT'::character varying, 'AUFTEILUNGSPLAN'::character varying, 'VORBEREITUNG_VORBESCHEID'::character varying, 'VORBEREITUNG_BAUGENEHMIGUNG'::character varying, 'VORABFRAGE_OHNE_KONKRETEN_STAND'::character varying, 'STRUKTURKONZEPT'::character varying, 'RAHMENPLANUNG'::character varying, 'POTENTIALUNTERSUCHUNG'::character varying, 'STAEDTEBAULICHE_SANIERUNGSMASSNAHME'::character varying, 'STAEDTEBAULICHE_ENTWICKLUNGSMASSNAHME'::character varying, 'INFO_FEHLT'::character varying, 'FREIE_EINGABE'::character varying, 'STANDORTABFRAGE'::character varying])::text[]))),
     ADD CONSTRAINT bauleitplanverfahren_status_abfrage_check CHECK (((status_abfrage)::text = ANY ((ARRAY['ANGELEGT'::character varying, 'OFFEN'::character varying, 'IN_BEARBEITUNG_SACHBEARBEITUNG'::character varying, 'IN_BEARBEITUNG_FACHREFERATE'::character varying, 'BEDARFSMELDUNG_ERFOLGT'::character varying, 'ERLEDIGT_MIT_FACHREFERAT'::character varying, 'ERLEDIGT_OHNE_FACHREFERAT'::character varying, 'ABBRUCH'::character varying])::text[])));
