00286fafc4616a161b93557536892f837297ab8e
tiago.kocevar <tiago.kocevar@muenchen.de>
2024-02-14 16:14:10 +0100

    Cast Error fix

--- src/main/java/de/muenchen/isi/infrastructure/repository/AbfragevarianteBaugenehmigungsverfahrenRepository.java
+++ src/main/java/de/muenchen/isi/infrastructure/repository/AbfragevarianteBaugenehmigungsverfahrenRepository.java
@@ -9,13 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 public interface AbfragevarianteBaugenehmigungsverfahrenRepository
     extends JpaRepository<AbfragevarianteBaugenehmigungsverfahren, UUID> {
     @Query(
-        value = "select abfragevarianten_baugenehmigungsverfahren_id from abfragevariante_baugenehmigungsverfahren where id::uuid = (:id)::uuid",
+        value = "select abfragevarianten_baugenehmigungsverfahren_id from abfragevariante_baugenehmigungsverfahren where CAST(id as uuid) = CAST(:id as uuid)",
         nativeQuery = true
     )
     Optional<UUID> findAbfrageIdForAbfragevarianteById(final UUID id);
 
     @Query(
-        value = "select abfragevarianten_sachbearbeitung_baugenehmigungsverfahren_id from abfragevariante_baugenehmigungsverfahren where id::uuid = (:id)::uuid",
+        value = "select abfragevarianten_sachbearbeitung_baugenehmigungsverfahren_id from abfragevariante_baugenehmigungsverfahren where CAST(:id as uuid) = CAST(:id as uuid)",
         nativeQuery = true
     )
     Optional<UUID> findAbfrageIdForAbfragevarianteSachbearbeitungById(final UUID id);
