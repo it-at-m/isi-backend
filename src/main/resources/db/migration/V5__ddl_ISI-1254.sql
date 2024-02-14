187dcf0f3485800f970ac0846335456903a35005
banzuu <103417158+banzuu@users.noreply.github.com>
2024-02-14 15:22:16 +0100

    Korrektur Migrationsskript

--- src/main/resources/db/migration/V5__ddl_ISI-1254.sql
+++ src/main/resources/db/migration/V5__ddl_ISI-1254.sql
@@ -203,8 +203,6 @@ ALTER TABLE IF EXISTS isidbuser.bauvorhaben
     ADD CONSTRAINT bauvorhaben_sobon_relevant_check1 CHECK (sobon_relevant::text = ANY (ARRAY['UNSPECIFIED'::character varying, 'TRUE'::character varying, 'FALSE'::character varying]::text[]));
 
 ALTER TABLE IF EXISTS isidbuser.bauvorhaben
-    DROP CONSTRAINT IF EXISTS bauvorhaben_stand_verfahren_check;
-ALTER TABLE IF EXISTS isidbuser.bauvorhaben
     ADD CONSTRAINT bauvorhaben_stand_verfahren_check CHECK (stand_verfahren::text = ANY (ARRAY['UNSPECIFIED'::character varying, 'VORBEREITUNG_ECKDATENBESCHLUSS'::character varying, 'VORBEREITUNG_WETTBEWERBAUSLOBUNG'::character varying, 'VORBEREITUNG_AUFSTELLUNGSBESCHLUSS'::character varying, 'VORBEREITUNG_BILLIGUNGSBESCHLUSS_STAEDTEBAULICHER_VERTRAG'::character varying, 'VORBEREITUNG_SATZUNGSBESCHLUSS'::character varying, 'VORLIEGENDER_SATZUNGSBESCHLUSS'::character varying, 'RECHTSVERBINDLICHKEIT_AMTSBLATT'::character varying, 'AUFTEILUNGSPLAN'::character varying, 'VORBEREITUNG_VORBESCHEID'::character varying, 'VORBEREITUNG_BAUGENEHMIGUNG'::character varying, 'VORABFRAGE_OHNE_KONKRETEN_STAND'::character varying, 'STRUKTURKONZEPT'::character varying, 'RAHMENPLANUNG'::character varying, 'POTENTIALUNTERSUCHUNG'::character varying, 'STAEDTEBAULICHE_SANIERUNGSMASSNAHME'::character varying, 'STAEDTEBAULICHE_ENTWICKLUNGSMASSNAHME'::character varying, 'INFO_FEHLT'::character varying, 'FREIE_EINGABE'::character varying, 'STANDORTABFRAGE'::character varying]::text[]));
 
 ALTER TABLE IF EXISTS isidbuser.bauvorhaben_art_fnp
@@ -232,8 +230,6 @@ ALTER TABLE IF EXISTS isidbuser.bauleitplanverfahren
 ALTER TABLE IF EXISTS isidbuser.bauleitplanverfahren
     ADD CONSTRAINT bauleitplanverfahren_sobon_relevant_check2 CHECK (sobon_relevant::text = ANY (ARRAY['UNSPECIFIED'::character varying, 'TRUE'::character varying, 'FALSE'::character varying]::text[]));
 
-ALTER TABLE IF EXISTS isidbuser.bauleitplanverfahren
-    DROP CONSTRAINT IF EXISTS bauleitplanverfahren_stand_verfahren_check;
 ALTER TABLE IF EXISTS isidbuser.bauleitplanverfahren
     ADD CONSTRAINT bauleitplanverfahren_stand_verfahren_check CHECK (stand_verfahren::text = ANY (ARRAY['UNSPECIFIED'::character varying, 'VORBEREITUNG_ECKDATENBESCHLUSS'::character varying, 'VORBEREITUNG_WETTBEWERBAUSLOBUNG'::character varying, 'VORBEREITUNG_AUFSTELLUNGSBESCHLUSS'::character varying, 'VORBEREITUNG_BILLIGUNGSBESCHLUSS_STAEDTEBAULICHER_VERTRAG'::character varying, 'VORBEREITUNG_SATZUNGSBESCHLUSS'::character varying, 'VORLIEGENDER_SATZUNGSBESCHLUSS'::character varying, 'RECHTSVERBINDLICHKEIT_AMTSBLATT'::character varying, 'AUFTEILUNGSPLAN'::character varying, 'VORBEREITUNG_VORBESCHEID'::character varying, 'VORBEREITUNG_BAUGENEHMIGUNG'::character varying, 'VORABFRAGE_OHNE_KONKRETEN_STAND'::character varying, 'STRUKTURKONZEPT'::character varying, 'RAHMENPLANUNG'::character varying, 'POTENTIALUNTERSUCHUNG'::character varying, 'STAEDTEBAULICHE_SANIERUNGSMASSNAHME'::character varying, 'STAEDTEBAULICHE_ENTWICKLUNGSMASSNAHME'::character varying, 'INFO_FEHLT'::character varying, 'FREIE_EINGABE'::character varying, 'STANDORTABFRAGE'::character varying]::text[]));
 
