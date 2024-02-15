e6a8eea048c461ef5f5b14eaba241d1ed08497ef
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-02-15 08:59:32 +0100

    add unit test

---
+++ src/test/java/de/muenchen/isi/domain/mapper/StammdatenDomainMapperTest.java
@@ -0,0 +1,49 @@
+package de.muenchen.isi.domain.mapper;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import de.muenchen.isi.infrastructure.csv.PrognosedatenKitaPlbCsv;
+import de.muenchen.isi.infrastructure.entity.enums.Altersgruppe;
+import de.muenchen.isi.infrastructure.entity.stammdaten.PrognoseKitaPlb;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class StammdatenDomainMapperTest {
+
+    private StammdatenDomainMapper stammdatenDomainMapper = new StammdatenDomainMapperImpl();
+
+    @Test
+    void csv2EntityForEachAltersgruppe() {
+        final var prognosedatenKitaPlbCsv = new PrognosedatenKitaPlbCsv();
+        prognosedatenKitaPlbCsv.setKitaPlb(99L);
+        prognosedatenKitaPlbCsv.setBerichtsstand(LocalDate.of(2024, 5, 1));
+        prognosedatenKitaPlbCsv.setAnzahlNullBisZweiJaehrige(BigDecimal.ONE);
+        prognosedatenKitaPlbCsv.setAnzahlDreiBisFuenfJaehrigeUndFuenfzigProzentSechsJaehrige(BigDecimal.TEN);
+
+        final var result = stammdatenDomainMapper.csv2EntityForEachAltersgruppe(prognosedatenKitaPlbCsv);
+
+        final var entity1 = new PrognoseKitaPlb();
+        entity1.setKitaPlb(99L);
+        entity1.setBerichtsstand(LocalDate.of(2024, 5, 1));
+        entity1.setAltersgruppe(Altersgruppe.NULL_ZWEI_JAEHRIGE);
+        entity1.setAnzahlKinder(BigDecimal.ONE);
+        final var entity2 = new PrognoseKitaPlb();
+        entity2.setKitaPlb(99L);
+        entity2.setBerichtsstand(LocalDate.of(2024, 5, 1));
+        entity2.setAltersgruppe(Altersgruppe.DREI_FUENF_UND_FUENFZIG_PROZENT_SECHS_JAEHRIGE);
+        entity2.setAnzahlKinder(BigDecimal.TEN);
+
+        final var expected = Stream.of(entity1, entity2);
+
+        assertThat(result.toList(), is(expected.toList()));
+    }
+}
