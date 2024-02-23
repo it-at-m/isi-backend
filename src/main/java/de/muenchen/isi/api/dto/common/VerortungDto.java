1fca9652bfb844b3d9e17ea2bc1310c8f8ecb405
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-02-23 15:26:06 +0100

    set attributes to nullable

--- src/main/java/de/muenchen/isi/api/dto/common/VerortungDto.java
+++ src/main/java/de/muenchen/isi/api/dto/common/VerortungDto.java
@@ -1,31 +1,23 @@
 package de.muenchen.isi.api.dto.common;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import java.util.Set;
 import lombok.Data;
 
 @Data
 public abstract class VerortungDto {
 
-    @NotNull
     private Set<@Valid StadtbezirkDto> stadtbezirke;
 
-    @NotNull
     private Set<@Valid BezirksteilDto> bezirksteile;
 
-    @NotNull
     private Set<@Valid ViertelDto> viertel;
 
-    @NotNull
     private Set<@Valid GemarkungDto> gemarkungen;
 
-    @NotNull
     private Set<@Valid KitaplanungsbereichDto> kitaplanungsbereiche;
 
-    @NotNull
     private Set<@Valid GrundschulsprengelDto> grundschulsprengel;
 
-    @NotNull
     private Set<@Valid MittelschulsprengelDto> mittelschulsprengel;
 }
