a409e27890757b8afc4a49beeebab32f5fa1adbb
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-03-21 16:25:12 +0100

    set basis baurateninput

--- src/main/java/de/muenchen/isi/domain/service/BauratendateiInputService.java
+++ src/main/java/de/muenchen/isi/domain/service/BauratendateiInputService.java
@@ -56,6 +56,7 @@ public class BauratendateiInputService {
                     abfragevariante.getId()
                 );
                 if (ObjectUtils.isNotEmpty(bauratendateiInput)) {
+                    abfragevariante.setBauratendateiInputBasis(bauratendateiInput);
                     final var bauratendateiInputs = ListUtils.emptyIfNull(abfragevariante.getBauratendateiInputs());
                     bauratendateiInputs.add(bauratendateiInput);
                     abfragevariante.setBauratendateiInputs(bauratendateiInputs);
@@ -76,6 +77,7 @@ public class BauratendateiInputService {
                     abfragevariante.getId()
                 );
                 if (ObjectUtils.isNotEmpty(bauratendateiInput)) {
+                    abfragevariante.setBauratendateiInputBasis(bauratendateiInput);
                     final var bauratendateiInputs = ListUtils.emptyIfNull(abfragevariante.getBauratendateiInputs());
                     bauratendateiInputs.add(bauratendateiInput);
                     abfragevariante.setBauratendateiInputs(bauratendateiInputs);
@@ -94,6 +96,7 @@ public class BauratendateiInputService {
                     abfragevariante.getId()
                 );
                 if (ObjectUtils.isNotEmpty(bauratendateiInput)) {
+                    abfragevariante.setBauratendateiInputBasis(bauratendateiInput);
                     final var bauratendateiInputs = ListUtils.emptyIfNull(abfragevariante.getBauratendateiInputs());
                     bauratendateiInputs.add(bauratendateiInput);
                     abfragevariante.setBauratendateiInputs(bauratendateiInputs);
