d3a7dd41da212871831cf00912d6209e8101661b
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-03-22 15:54:49 +0100

    add clone deep

--- src/main/java/de/muenchen/isi/domain/mapper/BauratendateiDomainMapper.java
+++ src/main/java/de/muenchen/isi/domain/mapper/BauratendateiDomainMapper.java
@@ -1,14 +1,19 @@
 package de.muenchen.isi.domain.mapper;
 
 import de.muenchen.isi.configuration.MapstructConfiguration;
+import de.muenchen.isi.domain.model.bauratendatei.BauratendateiInputModel;
 import de.muenchen.isi.domain.model.bauratendatei.BauratendateiWohneinheitenModel;
 import de.muenchen.isi.domain.model.calculation.WohneinheitenProFoerderartProJahrModel;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.control.DeepClone;
 
 @Mapper(config = MapstructConfiguration.class)
 public interface BauratendateiDomainMapper {
+    @DeepClone
+    BauratendateiInputModel cloneDeep(final BauratendateiInputModel model);
+
     @Mappings(
         {
             @Mapping(target = "id", ignore = true),
