8841db5c53caf3e6d11f54bd0a01146914808b1f
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-03-22 16:18:19 +0100

    change from entity realtion to element collection

--- src/main/java/de/muenchen/isi/infrastructure/entity/bauratendatei/BauratendateiInput.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/bauratendatei/BauratendateiInput.java
@@ -6,6 +6,7 @@ import jakarta.persistence.CollectionTable;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Index;
+import jakarta.persistence.OrderBy;
 import java.util.List;
 import java.util.Set;
 import lombok.Data;
@@ -28,6 +29,14 @@ public class BauratendateiInput extends BaseEntity {
     private Set<String> viertel;
 
     @ElementCollection
-    @CollectionTable(indexes = { @Index(name = "tbd", columnList = "tbd") })
+    @CollectionTable(
+        indexes = {
+            @Index(
+                name = "bauratendatei_input_wohneinheiten_bauratendatei_input_id",
+                columnList = "bauratendatei_input_id"
+            ),
+        }
+    )
+    @OrderBy("jahr asc, foerderart asc")
     private List<WohneinheitenProFoerderartProJahr> wohneinheiten;
 }
