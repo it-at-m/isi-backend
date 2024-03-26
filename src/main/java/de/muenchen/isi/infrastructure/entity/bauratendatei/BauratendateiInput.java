75854a7998325b52013cd5339c94e6f4f012caba
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-03-26 12:04:15 +0100

    add index to bauraten input file

--- src/main/java/de/muenchen/isi/infrastructure/entity/bauratendatei/BauratendateiInput.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/bauratendatei/BauratendateiInput.java
@@ -8,6 +8,7 @@ import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Index;
 import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
 import java.util.List;
 import java.util.Set;
 import lombok.Data;
@@ -20,6 +21,13 @@ import org.hibernate.type.SqlTypes;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @Entity
+@Table(
+    indexes = {
+        @Index(name = "bauratendatei_input_abfrgvar_baugnhmgsverfhrn_id", columnList = "abfrgvar_baugnhmgsverfhrn_id"),
+        @Index(name = "bauratendatei_input_abfrgvar_bauleitplnvrfhrn_id", columnList = "abfrgvar_bauleitplnvrfhrn_id"),
+        @Index(name = "bauratendatei_input_abfrgvar_weitrs_vrfhrn_id", columnList = "abfrgvar_weitrs_vrfhrn_id"),
+    }
+)
 public class BauratendateiInput extends BaseEntity {
 
     @JdbcTypeCode(SqlTypes.JSON)
