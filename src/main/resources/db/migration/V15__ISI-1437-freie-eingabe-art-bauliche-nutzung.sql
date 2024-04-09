3ec2e401c87983ff949e6b312d1b8f520693c20c
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-04-09 11:14:05 +0200

    fix sql script

--- src/main/resources/db/migration/V15__ISI-1437-freie-eingabe-art-bauliche-nutzung.sql
+++ src/main/resources/db/migration/V15__ISI-1437-freie-eingabe-art-bauliche-nutzung.sql
@@ -12,7 +12,7 @@ ALTER TABLE IF EXISTS isidbuser.baugebiet
 
 -- Hinzufügen der Freien Eingabe am Ende der Enums sowie des Eingabefeldes
 ALTER TABLE IF EXISTS isidbuser.baugebiet
-    ADD COLUMN art_bauliche_nutzung_freie_eingabe character varying(1000);
+    ADD COLUMN art_bauliche_nutzung_freie_eingabe character varying(1000),
     ADD CONSTRAINT baugebiet_art_bauliche_nutzung_check CHECK (art_bauliche_nutzung >= 0 AND art_bauliche_nutzung <= 8);
 
 
@@ -23,7 +23,7 @@ ALTER TABLE IF EXISTS isidbuser.bauvorhaben_art_fnp
     ADD DROP CONSTRAINT bauvorhaben_art_fnp_art_fnp_check;
 
 ALTER TABLE IF EXISTS isidbuser.bauvorhaben_art_fnp
-    ADD COLUMN art_fnp_freie_eingabe character varying(1000);
+    ADD COLUMN art_fnp_freie_eingabe character varying(1000),
     ADD CONSTRAINT bauvorhaben_art_fnp_art_fnp_check CHECK (art_fnp::text = ANY (ARRAY['UNSPECIFIED'::character varying, 'WR'::character varying, 'WA'::character varying, 'MU'::character varying, 'MK'::character varying, 'MI'::character varying, 'GE'::character varying, 'INFO_FEHLT'::character varying, 'FREIE_EINGABE'::character varying]::text[]));
 
 END;
