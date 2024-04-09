3c1abcafdf182fb2e2d0eea1570f9290ab77e949
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-04-09 14:06:56 +0200

    fix sql syntax error

--- src/main/resources/db/migration/V15__ISI-1437-freie-eingabe-art-bauliche-nutzung.sql
+++ src/main/resources/db/migration/V15__ISI-1437-freie-eingabe-art-bauliche-nutzung.sql
@@ -23,7 +23,9 @@ ALTER TABLE IF EXISTS isidbuser.bauvorhaben_art_fnp
     DROP CONSTRAINT bauvorhaben_art_fnp_art_fnp_check;
 
 ALTER TABLE IF EXISTS isidbuser.bauvorhaben_art_fnp
-    ADD COLUMN art_fnp_freie_eingabe character varying(1000),
     ADD CONSTRAINT bauvorhaben_art_fnp_art_fnp_check CHECK (art_fnp::text = ANY (ARRAY['WR'::character varying, 'WA'::character varying, 'MU'::character varying, 'MK'::character varying, 'MI'::character varying, 'GE'::character varying, 'INFO_FEHLT'::character varying, 'FREIE_EINGABE'::character varying]::text[]));
+
+ALTER TABLE IF EXISTS isidbuser.bauvorhaben
+    ADD COLUMN art_fnp_freie_eingabe character varying(1000);
 
 END;
