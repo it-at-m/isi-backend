2e3452ad116c5fc1848826536732bb789bb067c3
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-04-17 15:24:13 +0200

    adapt unit test

--- src/test/java/de/muenchen/isi/infrastructure/repository/email/MailSenderRepositoryTest.java
+++ src/test/java/de/muenchen/isi/infrastructure/repository/email/MailSenderRepositoryTest.java
@@ -1,7 +1,8 @@
 package de.muenchen.isi.infrastructure.repository.email;
 
-import de.muenchen.isi.domain.exception.ReportingException;
-import de.muenchen.isi.domain.service.email.SendWorkAssignmentInformationService;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,13 +11,15 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.MailParseException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({ MockitoExtension.class, OutputCaptureExtension.class })
 @MockitoSettings(strictness = Strictness.LENIENT)
 class MailSenderRepositoryTest {
 
@@ -45,7 +48,7 @@ class MailSenderRepositoryTest {
     }
 
     @Test
-    void sendMailMailSendException() {
+    void sendMailMailSendException(final CapturedOutput output) {
         final var mailMessage = new SimpleMailMessage();
         mailMessage.setFrom("fromEmailAddress");
         mailMessage.setTo("receiver");
@@ -56,11 +59,15 @@ class MailSenderRepositoryTest {
 
         mailSenderRepository.sendMail("receiver", "subject", "text");
 
+        assertThat(
+            output.getAll(),
+            containsString("MailSenderRepository -- Die Email konnte nicht an den Empfänger receiver versendet werden.")
+        );
         Mockito.verify(javaMailSender, Mockito.times(1)).send(mailMessage);
     }
 
     @Test
-    void sendMailMailAuthenticationException() {
+    void sendMailMailAuthenticationException(final CapturedOutput output) {
         final var mailMessage = new SimpleMailMessage();
         mailMessage.setFrom("fromEmailAddress");
         mailMessage.setTo("receiver");
@@ -71,11 +78,17 @@ class MailSenderRepositoryTest {
 
         mailSenderRepository.sendMail("receiver", "subject", "text");
 
+        assertThat(
+            output.getAll(),
+            containsString(
+                "MailSenderRepository -- Die Email konnte wegen fehlerhafter Emailcredentials nicht versendet werden."
+            )
+        );
         Mockito.verify(javaMailSender, Mockito.times(1)).send(mailMessage);
     }
 
     @Test
-    void sendMailMailParseException() {
+    void sendMailMailParseException(final CapturedOutput output) {
         final var mailMessage = new SimpleMailMessage();
         mailMessage.setFrom("fromEmailAddress");
         mailMessage.setTo("receiver");
@@ -86,11 +99,17 @@ class MailSenderRepositoryTest {
 
         mailSenderRepository.sendMail("receiver", "subject", "text");
 
+        assertThat(
+            output.getAll(),
+            containsString(
+                "MailSenderRepository -- Die Email konnte wegen fehlerhafter Email-Parameter nicht versendet werden."
+            )
+        );
         Mockito.verify(javaMailSender, Mockito.times(1)).send(mailMessage);
     }
 
     @Test
-    void sendMailException() {
+    void sendMailException(final CapturedOutput output) {
         final var mailMessage = new SimpleMailMessage();
         mailMessage.setFrom("fromEmailAddress");
         mailMessage.setTo("receiver");
@@ -101,6 +120,10 @@ class MailSenderRepositoryTest {
 
         mailSenderRepository.sendMail("receiver", "subject", "text");
 
+        assertThat(
+            output.getAll(),
+            containsString("MailSenderRepository -- Beim Emailversand ist ein Fehler aufgetreten.")
+        );
         Mockito.verify(javaMailSender, Mockito.times(1)).send(mailMessage);
     }
 }
