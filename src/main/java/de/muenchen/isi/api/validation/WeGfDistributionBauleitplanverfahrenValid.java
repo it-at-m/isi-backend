df3c7b6486f3b3c7f67b2289ff2a48038671b6fe
banzuu <103417158+banzuu@users.noreply.github.com>
2024-04-18 14:28:17 +0200

    ISI-1466 Korrektur für Validierung der Verteilung von Wohneinheiten und Geschossfläche (#158)
    * Implementierung
    
    * Kommentare

---
+++ src/main/java/de/muenchen/isi/api/validation/WeGfDistributionBauleitplanverfahrenValid.java
@@ -0,0 +1,18 @@
+package de.muenchen.isi.api.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = WeGfDistributionBauleitplanverfahrenValidator.class)
+@Documented
+public @interface WeGfDistributionBauleitplanverfahrenValid {
+    String message() default "Die Summe der über die Baugebiete verteilten Wohneinheiten oder Geschossfläche Wohnen" +
+    " entspricht nicht der Anzahl der Wohneinheiten oder Geschossfläche Wohnen in der Abfragevariante.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
