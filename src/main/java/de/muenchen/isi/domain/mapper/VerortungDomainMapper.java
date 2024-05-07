2b8e52abe2d5af4193b5d2a45467169c9f2f66b1
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-05-07 14:56:25 +0200

    sorround with exception

--- src/main/java/de/muenchen/isi/domain/mapper/VerortungDomainMapper.java
+++ src/main/java/de/muenchen/isi/domain/mapper/VerortungDomainMapper.java
@@ -1,6 +1,8 @@
 package de.muenchen.isi.domain.mapper;
 
 import de.muenchen.isi.configuration.MapstructConfiguration;
+import de.muenchen.isi.domain.exception.GeometryOperationFailedException;
+import de.muenchen.isi.domain.exception.KoordinatenException;
 import de.muenchen.isi.domain.model.common.VerortungMultiPolygonModel;
 import de.muenchen.isi.domain.model.common.VerortungPointModel;
 import de.muenchen.isi.domain.service.KoordinatenService;
@@ -33,27 +35,32 @@ public abstract class VerortungDomainMapper {
     public abstract VerortungMultiPolygon model2Entity(final VerortungMultiPolygonModel model);
 
     @AfterMapping
-    @SneakyThrows
     public void afterMappingModel2Entity(
         final VerortungMultiPolygonModel model,
         @MappingTarget final VerortungMultiPolygon entity
     ) {
-        if (ObjectUtils.isNotEmpty(entity.getMultiPolygon())) {
-            final var centroidWgs84 = koordinatenService.getMultiPolygonCentroid(entity.getMultiPolygon());
-            final var centroidWgs84Model = koordinatenDomainMapper.entity2Model(centroidWgs84);
-            final var centroidPointGeometry = new PointGeometry();
-            centroidPointGeometry.setType(TYPE_POINT_GEOMETRY);
-            centroidPointGeometry.setCoordinates(
-                List.of(
-                    BigDecimal.valueOf(centroidWgs84.getLongitude()),
-                    BigDecimal.valueOf(centroidWgs84.getLatitude())
-                )
-            );
-            entity.setCentroid(centroidPointGeometry);
+        try {
+            if (ObjectUtils.isNotEmpty(entity.getMultiPolygon())) {
+                final var centroidWgs84 = koordinatenService.getMultiPolygonCentroid(entity.getMultiPolygon());
+                final var centroidWgs84Model = koordinatenDomainMapper.entity2Model(centroidWgs84);
+                final var centroidPointGeometry = new PointGeometry();
+                centroidPointGeometry.setType(TYPE_POINT_GEOMETRY);
+                centroidPointGeometry.setCoordinates(
+                    List.of(
+                        BigDecimal.valueOf(centroidWgs84.getLongitude()),
+                        BigDecimal.valueOf(centroidWgs84.getLatitude())
+                    )
+                );
+                entity.setCentroid(centroidPointGeometry);
 
-            final var centroidUtmModel = koordinatenService.wgs84ToUtm32(centroidWgs84Model);
-            final var centroidUtm = koordinatenDomainMapper.model2Entity(centroidUtmModel);
-            entity.setCentroidUtm(centroidUtm);
+                final var centroidUtmModel = koordinatenService.wgs84ToUtm32(centroidWgs84Model);
+                final var centroidUtm = koordinatenDomainMapper.model2Entity(centroidUtmModel);
+                entity.setCentroidUtm(centroidUtm);
+            }
+        } catch (final GeometryOperationFailedException | KoordinatenException exception) {
+            entity.setCentroid(null);
+            entity.setCentroidUtm(null);
+            log.error("Für die Verortung eines Multipolygons konnte kein Schwerpunkt ermittelt werden.", exception);
         }
     }
 
@@ -62,18 +69,25 @@ public abstract class VerortungDomainMapper {
     public abstract VerortungPoint model2Entity(final VerortungPointModel model);
 
     @AfterMapping
-    @SneakyThrows
     public void afterMappingModel2Entity(final VerortungPointModel model, @MappingTarget final VerortungPoint entity) {
-        if (ObjectUtils.isNotEmpty(entity.getPoint())) {
-            final var longitude = entity.getPoint().getCoordinates().get(0).doubleValue();
-            final var latitude = entity.getPoint().getCoordinates().get(1).doubleValue();
-            final var pointWgs84 = new Wgs84();
-            pointWgs84.setLongitude(longitude);
-            pointWgs84.setLatitude(latitude);
-            final var pointWgs84Model = koordinatenDomainMapper.entity2Model(pointWgs84);
-            final var pointUtmModel = koordinatenService.wgs84ToUtm32(pointWgs84Model);
-            final var pointUtm = koordinatenDomainMapper.model2Entity(pointUtmModel);
-            entity.setPointUtm(pointUtm);
+        try {
+            if (ObjectUtils.isNotEmpty(entity.getPoint())) {
+                final var longitude = entity.getPoint().getCoordinates().get(0).doubleValue();
+                final var latitude = entity.getPoint().getCoordinates().get(1).doubleValue();
+                final var pointWgs84 = new Wgs84();
+                pointWgs84.setLongitude(longitude);
+                pointWgs84.setLatitude(latitude);
+                final var pointWgs84Model = koordinatenDomainMapper.entity2Model(pointWgs84);
+                final var pointUtmModel = koordinatenService.wgs84ToUtm32(pointWgs84Model);
+                final var pointUtm = koordinatenDomainMapper.model2Entity(pointUtmModel);
+                entity.setPointUtm(pointUtm);
+            }
+        } catch (final KoordinatenException exception) {
+            entity.setPointUtm(null);
+            log.error(
+                "Für die Verortung einer Punktkoordinate konnte keine UTM-Transformation durchgeführt werden.",
+                exception
+            );
         }
     }
 
