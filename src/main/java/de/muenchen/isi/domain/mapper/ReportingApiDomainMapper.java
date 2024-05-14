551a3e5ef7e6b5e3c329c7f016d3692ebe26646a
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-05-14 17:20:31 +0200

    adapt mapping for reporting

--- src/main/java/de/muenchen/isi/domain/mapper/ReportingApiDomainMapper.java
+++ src/main/java/de/muenchen/isi/domain/mapper/ReportingApiDomainMapper.java
@@ -37,6 +37,7 @@ import org.mapstruct.SubclassMapping;
 public interface ReportingApiDomainMapper {
     @Mapping(target = "createdDateTime", ignore = true)
     @Mapping(target = "lastModifiedDateTime", ignore = true)
+    @Mapping(source = "bauvorhaben", target = "bauvorhabenId")
     @SubclassMapping(source = BauleitplanverfahrenModel.class, target = BauleitplanverfahrenDto.class)
     @SubclassMapping(source = BaugenehmigungsverfahrenModel.class, target = BaugenehmigungsverfahrenDto.class)
     @SubclassMapping(source = WeiteresVerfahrenModel.class, target = WeiteresVerfahrenDto.class)
