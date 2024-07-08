3c3774fc98f7f6a9e8c142ff6c75baa32bbcfb7c
tiago.kocevar <tiago.kocevar@muenchen.de>
2024-07-08 16:52:05 +0200

    Reports removed

--- src/main/java/de/muenchen/isi/domain/service/stammdaten/MetabaseReportingService.java
+++ src/main/java/de/muenchen/isi/domain/service/stammdaten/MetabaseReportingService.java
@@ -11,43 +11,27 @@ public class MetabaseReportingService {
 
     private final String url;
 
-    private final String reportBedarfe;
-
     private final String reportsGlobal;
 
     private final String reportErgebnissePlanungsursaechlich;
 
     private final String reportErgebnisseSobonUrsaechlich;
 
-    private final String reportSpitzenbedarfePlanungsursaechlich;
-
-    private final String reportSpitzenbedarfeSobonUrsaechlich;
-
     private final String reportWohneinheiten;
 
     public MetabaseReportingService(
         @Value("${metabase.reporting.url}") final String url,
         @Value("${metabase.reporting.reports.global}") final String reportsGlobal,
-        @Value("${metabase.reporting.reports.report.bedarfe}") final String reportBedarfe,
         @Value(
             "${metabase.reporting.reports.report.ergebnisse.planungsursaechlich}"
         ) final String reportErgebnissePlanungsursaechlich,
         @Value(
             "${metabase.reporting.reports.report.ergebnisse.sobonursaechlich}"
         ) final String reportErgebnisseSobonUrsaechlich,
-        @Value(
-            "${metabase.reporting.reports.report.spitzenbedarfe.planungsursaechlich}"
-        ) final String reportSpitzenbedarfePlanungsursaechlich,
-        @Value(
-            "${metabase.reporting.reports.report.spitzenbedarfe.sobon.ursaechlich}"
-        ) final String reportSpitzenbedarfeSobonUrsaechlich,
         @Value("${metabase.reporting.reports.report.wohneinheiten}") final String reportWohneinheiten
     ) {
         this.url = url;
         this.reportsGlobal = reportsGlobal;
-        this.reportBedarfe = reportBedarfe;
-        this.reportSpitzenbedarfePlanungsursaechlich = reportSpitzenbedarfePlanungsursaechlich;
-        this.reportSpitzenbedarfeSobonUrsaechlich = reportSpitzenbedarfeSobonUrsaechlich;
         this.reportWohneinheiten = reportWohneinheiten;
         this.reportErgebnissePlanungsursaechlich = reportErgebnissePlanungsursaechlich;
         this.reportErgebnisseSobonUrsaechlich = reportErgebnisseSobonUrsaechlich;
@@ -60,11 +44,8 @@ public class MetabaseReportingService {
         final var metabaseReportingModel = new MetabaseReportingModel();
         metabaseReportingModel.setUrl(this.url);
         metabaseReportingModel.setReportsGlobal(this.reportsGlobal);
-        metabaseReportingModel.setReportBedarfe(this.reportBedarfe);
         metabaseReportingModel.setReportErgebnissePlanungsursaechlich(this.reportErgebnissePlanungsursaechlich);
         metabaseReportingModel.setReportErgebnisseSobonUrsaechlich(this.reportErgebnisseSobonUrsaechlich);
-        metabaseReportingModel.setReportSpitzenbedarfePlanungsursaechlich(this.reportSpitzenbedarfePlanungsursaechlich);
-        metabaseReportingModel.setReportSpitzenbedarfeSobonUrsaechlich(this.reportSpitzenbedarfeSobonUrsaechlich);
         metabaseReportingModel.setReportWohneinheiten(this.reportWohneinheiten);
         return metabaseReportingModel;
     }
