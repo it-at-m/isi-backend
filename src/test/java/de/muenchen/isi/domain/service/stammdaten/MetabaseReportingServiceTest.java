163399dc9f65f26201604e071fbceaac5f61fc33
Tiago <62375155+TiagoKocevar@users.noreply.github.com>
2024-07-25 08:21:54 +0200

    Reports removed (#178)
    Co-authored-by: alexander.boxhorn <alexander.boxhorn@muenchen.de>

--- src/test/java/de/muenchen/isi/domain/service/stammdaten/MetabaseReportingServiceTest.java
+++ src/test/java/de/muenchen/isi/domain/service/stammdaten/MetabaseReportingServiceTest.java
@@ -23,11 +23,8 @@ class MetabaseReportingServiceTest {
             new MetabaseReportingService(
                 "https://isi-metabase-test.muenchen.de",
                 "collection/99-global-reports",
-                "dashboard/45-dashboard-bedarfe",
                 " dashboard/36-ergebnisse-der-planungsursachlichen-bedarfsberechnung",
                 "dashboard/44-ergebnisse-der-sobon-bedarfsberechnung",
-                "dashboard/27-dashboard-spitzenbedarfe-planungsursachlich",
-                "dashboard/30-dashboard-spitzenbedarfe-sobon-ursachlich",
                 "dashboard/33-dashboard-wohneinheiten"
             );
     }
@@ -37,13 +34,10 @@ class MetabaseReportingServiceTest {
         final var expected = new MetabaseReportingModel();
         expected.setUrl("https://isi-metabase-test.muenchen.de");
         expected.setReportsGlobal("collection/99-global-reports");
-        expected.setReportBedarfe("dashboard/45-dashboard-bedarfe");
         expected.setReportErgebnissePlanungsursaechlich(
             " dashboard/36-ergebnisse-der-planungsursachlichen-bedarfsberechnung"
         );
         expected.setReportErgebnisseSobonUrsaechlich("dashboard/44-ergebnisse-der-sobon-bedarfsberechnung");
-        expected.setReportSpitzenbedarfePlanungsursaechlich("dashboard/27-dashboard-spitzenbedarfe-planungsursachlich");
-        expected.setReportSpitzenbedarfeSobonUrsaechlich("dashboard/30-dashboard-spitzenbedarfe-sobon-ursachlich");
         expected.setReportWohneinheiten("dashboard/33-dashboard-wohneinheiten");
         assertThat(this.metabaseReportingService.getMetabaseReporting(), is(expected));
     }
