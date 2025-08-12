34ad9d88d77fac6cf149f1504d07d2964e9b74bd
TiagoKocevar <tiago.kocevar@gmail.com>
2025-08-12 09:31:54 +0200

    Remove unnecessary code

--- src/main/java/de/muenchen/isi/domain/mapper/AbfrageDomainMapper.java
+++ src/main/java/de/muenchen/isi/domain/mapper/AbfrageDomainMapper.java
@@ -69,7 +69,6 @@ public abstract class AbfrageDomainMapper {
     @SubclassMapping(source = Baugenehmigungsverfahren.class, target = BaugenehmigungsverfahrenModel.class)
     @SubclassMapping(source = WeiteresVerfahren.class, target = WeiteresVerfahrenModel.class)
     @Mapping(source = "bauvorhaben.id", target = "bauvorhaben")
-    @Mapping(source = "artAbfrage", target = "artAbfrage")
     public abstract AbfrageModel entity2Model(final Abfrage entity);
 
     @SubclassMapping(source = BauleitplanverfahrenModel.class, target = Bauleitplanverfahren.class)
