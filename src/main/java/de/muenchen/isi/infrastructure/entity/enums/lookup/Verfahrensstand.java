31704bc6b0d17c6ad83addc2885d5e006a94b435
albrecht.schaenzel <albrecht.schaenzel@muenchen.de>
2026-05-08 13:37:22 +0200

    Verfahrensstand für Baugenehmigungsverfahren angepasst

--- src/main/java/de/muenchen/isi/infrastructure/entity/enums/lookup/Verfahrensstand.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/enums/lookup/Verfahrensstand.java
@@ -40,7 +40,17 @@ public enum Verfahrensstand implements ILookup {
 
     INKRAFTGETRETEN_FOERDERMIXPLAN("Fördermixplan", new String[] { "Fördermixplan" }),
 
-    /* ab hier To-Do: ISI-273, ISI-273 */
+    VORBEREITUNG_VORBESCHEID(
+        "Bauvoranfrage, Art. 71 BayBO (Vorbereitung Vorbescheid)",
+        new String[] { "Bauvoranfrage", "Vorbereitung", "Vorbescheid" }
+    ),
+
+    VORBEREITUNG_BAUGENEHMIGUNG(
+        "Genehmigungsverfahren, Art. 59 / Art. 60 BayBO (Vorbereitung Baugenehmigung)",
+        new String[] { "Genehmigungsverfahren", "Vorbereitung", "Baugenehmigung" }
+    ),
+
+    /* ab hier To-Do: ISI-280 */
 
     VORBEREITUNG_ECKDATENBESCHLUSS(
         "Vorbereitung Eckdatenbeschluss",
@@ -68,10 +78,6 @@ public enum Verfahrensstand implements ILookup {
     ),
 
     AUFTEILUNGSPLAN("Aufteilungsplan", new String[] { "Aufteilungsplan" }),
-
-    VORBEREITUNG_VORBESCHEID("Vorbereitung Vorbescheid", new String[] { "Vorbereitung", "Vorbescheid" }),
-
-    VORBEREITUNG_BAUGENEHMIGUNG("Vorbereitung Baugenehmigung", new String[] { "Vorbereitung", "Baugenehmigung" }),
 
     VORABFRAGE_OHNE_KONKRETEN_STAND(
         "Vorabfrage ohne konkreten Stand",
