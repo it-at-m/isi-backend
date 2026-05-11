1d71c7023b69e7de2c33571cba742323a5a4e1e4
albrecht.schaenzel <albrecht.schaenzel@muenchen.de>
2026-05-11 12:59:19 +0200

    Migration Wesentliche Rechtsgrundlage

--- src/main/java/de/muenchen/isi/infrastructure/entity/enums/lookup/WesentlicheRechtsgrundlage.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/enums/lookup/WesentlicheRechtsgrundlage.java
@@ -6,6 +6,24 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum WesentlicheRechtsgrundlage implements ILookup {
+    BEPLANTER_BEREICH_PARAGRAPH_30("Beplanter Bereich (§ 30 BauGB)"),
+
+    INNENBEREICH("Innenbereich (§34 BauGB)"),
+
+    AUSSENBEREICH("Außenbereich (§35 BauGB)"),
+
+    BEBAUUNGSPLAN_ZUR_WOHNRAUMVERSORGUNG_PARAGRAPH_9_IVM_34(
+        "Bebauungsplan zur Wohnraumversorgung (§ 9 Abs. 2d i.V.m. § 34 BauGB)"
+    ),
+
+    BEPLANTER_BEREICH_PARAGRAPH_30_MIT_BEFREIUNG_PARAGRAPH_31(
+        "Beplanter Bereich (§30 BauGB) mit Befreiung (§31 BauGB)"
+    ),
+
+    FREIE_EINGABE("freie Eingabe"),
+
+    /* ab hier ISI-280) */
+
     QUALIFIZIERTER_BEBAUUNGSPLAN("Qualifizierter Bebauungsplan (§30 Abs. 1 BauGB)"),
 
     VORHABENSBEZOGENER_BEBAUUNGSPLAN("Vorhabenbezogener Bebauungsplan (§12 BauGB, § 30 Abs. 2 BauGB)"),
@@ -21,26 +39,18 @@ public enum WesentlicheRechtsgrundlage implements ILookup {
         "Sektoraler Bebauungsplan (§30 Abs. 3 BauGB i.V.m. §34 BauGB oder i.V.m. §35 BauGB)"
     ),
 
-    INNENBEREICH("Innenbereich (§34 BauGB)"),
-
-    AUSSENBEREICH("Außenbereich (§35 BauGB)"),
-
-    BEFREIUNG("Befreiung (§ 31 BauGB)"),
-
-    FREIE_EINGABE("freie Eingabe");
+    BEFREIUNG("Befreiung (§ 31 BauGB)");
 
     @Getter
     private final String bezeichnung;
 
     public static List<WesentlicheRechtsgrundlage> getWesentlicheRechtsgrundlageForBaugenehmigungsverfahren() {
         return List.of(
-            QUALIFIZIERTER_BEBAUUNGSPLAN,
-            VORHABENSBEZOGENER_BEBAUUNGSPLAN,
-            EINFACHER_BEBAUUNGSPLAN_PARAGRAPH_30_IVM_34_35,
-            SEKTORALER_BEBAUUNGSPLAN_PARAGRAPH_30_IVM_34_35,
+            BEPLANTER_BEREICH_PARAGRAPH_30,
             INNENBEREICH,
             AUSSENBEREICH,
-            BEFREIUNG,
+            BEBAUUNGSPLAN_ZUR_WOHNRAUMVERSORGUNG_PARAGRAPH_9_IVM_34,
+            BEPLANTER_BEREICH_PARAGRAPH_30_MIT_BEFREIUNG_PARAGRAPH_31,
             FREIE_EINGABE
         );
     }
