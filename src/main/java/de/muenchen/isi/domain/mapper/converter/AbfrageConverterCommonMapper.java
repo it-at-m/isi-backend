ab7896e257eb386f965d9ee7b761b5001aa70097
albrecht.schaenzel <albrecht.schaenzel@muenchen.de>
2026-08-12 11:13:41 +0200

    clean imports

--- src/main/java/de/muenchen/isi/domain/mapper/converter/AbfrageConverterCommonMapper.java
+++ src/main/java/de/muenchen/isi/domain/mapper/converter/AbfrageConverterCommonMapper.java
@@ -1,11 +1,9 @@
 package de.muenchen.isi.domain.mapper.converter;
 
-import de.muenchen.isi.configuration.MapstructConfiguration;
 import de.muenchen.isi.domain.model.AbfrageModel;
 import de.muenchen.isi.infrastructure.entity.enums.lookup.StatusAbfrage;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(config = AbfrageConverterMapperConfig.class)
