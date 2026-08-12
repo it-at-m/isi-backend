7ce6e70a59084769827d3fef921c9ddc27582de4
albrecht.schaenzel <albrecht.schaenzel@muenchen.de>
2026-08-12 11:50:37 +0200

    ignoreCommonFields bei Abfragevariante ergänzt

--- src/main/java/de/muenchen/isi/domain/mapper/converter/AbfrageConverterMapperConfig.java
+++ src/main/java/de/muenchen/isi/domain/mapper/converter/AbfrageConverterMapperConfig.java
@@ -27,5 +27,8 @@ public interface AbfrageConverterMapperConfig {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdDateTime", ignore = true)
+    @Mapping(target = "lastModifiedDateTime", ignore = true)
+    @Mapping(target = "artAbfrage", ignore = true)
     void ignoreCommonFields(AbfragevarianteModel source, @MappingTarget AbfragevarianteModel target);
 }
