e10a2d3aead9426a4a9ae6e2b087a3e5f947eb91
danielgully <74965377+danielgully@users.noreply.github.com>
2026-09-02 15:50:56 +0200

    refactor: remove unused imports

--- src/main/java/de/muenchen/isi/infrastructure/entity/search/filter/PersonalFilter.java
+++ src/main/java/de/muenchen/isi/infrastructure/entity/search/filter/PersonalFilter.java
@@ -1,18 +1,14 @@
 package de.muenchen.isi.infrastructure.entity.search.filter;
 
 import de.muenchen.isi.infrastructure.entity.BaseEntity;
-import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
-import java.util.UUID;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
