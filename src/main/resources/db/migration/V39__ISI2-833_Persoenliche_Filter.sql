5c45aef5d576363e28b53e8424fc33d158d33718
danielgully <74965377+danielgully@users.noreply.github.com>
2026-09-04 15:33:36 +0200

    feat: add select- and sort-attributes in migration

--- src/main/resources/db/migration/V39__ISI2-833_Persoenliche_Filter.sql
+++ src/main/resources/db/migration/V39__ISI2-833_Persoenliche_Filter.sql
@@ -6,23 +6,35 @@ BEGIN;
 
 CREATE TABLE isidbuser.personal_filter
 (
-    id                      character varying(36) PRIMARY KEY,
-    version                 BIGINT,
-    created_date_time       TIMESTAMP              NOT NULL,
-    last_modified_date_time TIMESTAMP              NOT NULL,
+    id                              character varying(36) PRIMARY KEY,
+    version                         BIGINT,
+    created_date_time               TIMESTAMP              NOT NULL,
+    last_modified_date_time         TIMESTAMP              NOT NULL,
 
-    personalid              character varying(255) NOT NULL,
-    filter_name             character varying(255) NOT NULL,
+    personalid                      character varying(255) NOT NULL,
+    filter_name                     character varying(255) NOT NULL,
 
     -- Embedded FilterSettings
-    realisierungsbeginn_von INTEGER,
-    realisierungsbeginn_bis INTEGER,
-    nur_eigene_abfragen     BOOLEAN,
-    sobon_relevant          character varying(255) NOT NULL,
-    we_gesamt_von           INTEGER,
-    we_gesamt_bis           INTEGER,
-    gf_wohnen_geplant_von   DECIMAL(19, 2),
-    gf_wohnen_geplant_bis   DECIMAL(19, 2),
+    sort_by                         character varying(255) NOT NULL,
+    sort_order                      character varying(255) NOT NULL,
+    select_bauleitplanverfahren     BOOLEAN                NOT NULL,
+    select_baugenehmigungsverfahren BOOLEAN                NOT NULL,
+    select_weiteres_verfahren       BOOLEAN                NOT NULL,
+    select_bauvorhaben              BOOLEAN                NOT NULL,
+    select_grundschule              BOOLEAN                NOT NULL,
+    select_gs_nachmittag_betreuung  BOOLEAN                NOT NULL,
+    select_haus_fuer_kinder         BOOLEAN                NOT NULL,
+    select_kindergarten             BOOLEAN                NOT NULL,
+    select_kinderkrippe             BOOLEAN                NOT NULL,
+    select_mittelschule             BOOLEAN                NOT NULL,
+    realisierungsbeginn_von         INTEGER,
+    realisierungsbeginn_bis         INTEGER,
+    nur_eigene_abfragen             BOOLEAN,
+    sobon_relevant                  character varying(255) NOT NULL,
+    we_gesamt_von                   INTEGER,
+    we_gesamt_bis                   INTEGER,
+    gf_wohnen_geplant_von           DECIMAL(19, 2),
+    gf_wohnen_geplant_bis           DECIMAL(19, 2),
     CONSTRAINT personal_filter_sobon_relevant_check CHECK (((sobon_relevant)::text <> 'UNSPECIFIED'::text))
 );
 
