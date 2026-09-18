61c6535fef64721970a607986b8cca128b71ad37
danielgully <74965377+danielgully@users.noreply.github.com>
2026-09-18 12:23:12 +0200

    refactor: correct endpoint-mapping

--- src/main/java/de/muenchen/isi/api/controller/filter/PersonalFilterController.java
+++ src/main/java/de/muenchen/isi/api/controller/filter/PersonalFilterController.java
@@ -28,9 +28,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -117,7 +117,7 @@ public class PersonalFilterController {
         }
     )
     @Transactional(rollbackFor = OptimisticLockingException.class)
-    @PatchMapping("/edit")
+    @PutMapping("/edit")
     public PersonalFilterResponseDto editFilter(
         @RequestBody @Valid @NotNull PersonalFilterRequestDto personalFilterRequestDto
     ) throws OptimisticLockingException, EntityNotFoundException, UserRoleNotAllowedException, NotOwnerException {
